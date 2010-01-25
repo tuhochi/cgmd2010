b469fe8e71f775e48f507570d1e283a1cdd06c90
peter.rautek <peter.rautek@77361afc-002d-11df-9d03-c52165109085>
2010-01-25 18:30:01 +0000

    - first draft of map activity - added levels

---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level04/GlActivity.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level04;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+
+public class GlActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.l04_main);
+    }
+}
