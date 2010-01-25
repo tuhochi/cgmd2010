a4071cef4ac6166c8c0fa54ebfa3b00c7524fa42
peter.rautek <peter.rautek@77361afc-002d-11df-9d03-c52165109085>
2010-01-25 18:30:01 +0000

    - first draft of map activity - added levels

--- src/at/ac/tuwien/cg/cgmd/bifth2010/Constants.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/Constants.java
@@ -2,4 +2,6 @@ package at.ac.tuwien.cg.cgmd.bifth2010;
 
 public final class Constants {
 	public static final String PACKAGE_NAME = "at.ac.tuwien.cg.cgmd.bifth2010";
+	public static final String RETURN_VALUE_POINTS = "points";
+	public static final int NUMBER_OF_LEVELS = 7;
 }
--- src/at/ac/tuwien/cg/cgmd/bifth2010/framework/MapActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/framework/MapActivity.java
@@ -1,20 +1,23 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.framework;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
+import android.widget.AbsoluteLayout;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import at.ac.tuwien.cg.cgmd.bifth2010.Constants;
 import at.ac.tuwien.cg.cgmd.bifth2010.R;
+import at.ac.tuwien.cg.cgmd.bifth2010.framework.Path.PathPoint;
 
 /**
  * 
@@ -28,70 +31,189 @@ public class MapActivity extends Activity {
 	
 	private static final String CLASS_TAG = MapActivity.class.getName();
 	
-	private MapView mMapView;
-	private TextView mStatusText;
-	private String mFpsString;
+	private TextView mDebugTextView;
+	//private String mDebugText;
+	private Path mPath = new Path();
+	private float mProgress = 0;
+	private ImageButton mPlayer = null;
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //make fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);  
+        
+        // set the layout
         setContentView(R.layout.l00_map);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.l00_LinearLayout);
-        mStatusText = (TextView) findViewById(R.id.l00_TextViewStatus); 
         
-         /*
-        //TODO add buttons and listeners for each level 
-        ImageButton buttonStartLevel01 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel01);
-        buttonStartLevel01.setOnClickListener(mLevelClickListener01);
-        ImageButton buttonStartLevel02 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel02);
-        buttonStartLevel02.setOnClickListener(mLevelClickListener02);
-        */
+        // get the frame layout
+        FrameLayout fl = (FrameLayout) findViewById(R.id.l00_FrameLayout);
         
-        mMapView = new MapView(this);
-        ll.addView(mMapView);
-        
-        Timer mFpsUpdateTimer = new Timer();
-        mFpsUpdateTimer.schedule(new TimerTask(){
+        AbsoluteLayout absoluteLayout = (AbsoluteLayout) findViewById(R.id.l00_AbsoluteLayout);
+        RelativePositionLayout relativePositionLayout = new RelativePositionLayout(this);
+  
+        relativePositionLayout.setBackgroundResource(R.drawable.l00_map_landscape);
+        Drawable d = relativePositionLayout.getBackground();;
+        int iSize = absoluteLayout.getChildCount();
 
-			@Override
-			public void run() {
-				handleUIChanges.sendEmptyMessage(0);
-				float fps = mMapView.getFps();				
-				mFpsString = "fps: " + Float.toString(fps);
-			}
-        }, 1000, 1000);
+        // we convert the layouting coordinates into relative coordinates and exchange the absolute view with a relativepositioning view 
+        float fW = 480;
+        float fH = 270;
+        String s ="";
+        for(int i=0; i<iSize; i++) {
+        	View v = absoluteLayout.getChildAt(0);
+        	AbsoluteLayout.LayoutParams lp = (AbsoluteLayout.LayoutParams) v.getLayoutParams(); 
+        	float fX = ((float)lp.x) / fW * 100.f;
+        	float fY = ((float)lp.y) / fH * 100.f;
+        	
+        	s += "Point p = new PathPoint(" + Float.toString(fX) + ", " + Float.toString(fY) + ")";  
+        	
+        	absoluteLayout.removeView(v);
+        	lp.x = (int) fX;
+        	lp.y = (int) fY;
+        	relativePositionLayout.addView(v);        	
+        }
+        Log.d(CLASS_TAG, s);
+        
+        fl.removeView(absoluteLayout);
+        fl.addView(relativePositionLayout);
+        
+        mPlayer = (ImageButton) findViewById(R.id.l00_ImageButtonPlayer);
+        
+        ImageButton l00 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel00);
+        ImageButton l01 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel01);
+        ImageButton l02 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel02);
+        ImageButton l03 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel03);
+        ImageButton l04 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel04);
+        ImageButton l05 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel05);
+        ImageButton l06 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel06);
+        ImageButton l07 = (ImageButton) findViewById(R.id.l00_ImageButtonLevel07);
+        
+        l00.setOnClickListener(mLevelClickListener);
+        l01.setOnClickListener(mLevelClickListener);
+        l02.setOnClickListener(mLevelClickListener);
+        l03.setOnClickListener(mLevelClickListener);
+        l04.setOnClickListener(mLevelClickListener);
+        l05.setOnClickListener(mLevelClickListener);
+        l06.setOnClickListener(mLevelClickListener);
+        l07.setOnClickListener(mLevelClickListener);
+        
+        // get the debug text view
+        mDebugTextView = (TextView) findViewById(R.id.l00_TextViewFps);
     }
     
-    private Handler handleUIChanges = new Handler(){
-    	@Override
-    	public void handleMessage(Message msg) {
-    		super.handleMessage(msg);
-    		mStatusText.setText(mFpsString);
-			//mStatusText.invalidate();
-    	}
+    private Handler mUiUpdateHandler = new Handler(){
+    	
+    	public void handleMessage(android.os.Message msg) {
+    		PathPoint p = mPath.interpolate(mProgress);
+    		AbsoluteLayout.LayoutParams lp = (AbsoluteLayout.LayoutParams) mPlayer.getLayoutParams();
+        	lp.x = (int) p.mX;
+        	lp.y = (int) p.mY;
+        	mDebugTextView.setText("x: " + p.mX + ",y: "+ p.mY);
+        	mPlayer.invalidate();
+    	};
     };
+   
     
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mMapView.onPause();
-    }
+    private OnClickListener mLevelClickListener = new OnClickListener(){
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
-    
-    private OnClickListener mLevelClickListener01 = new OnClickListener(){
-
-		@Override
+    	@Override
 		public void onClick(View v) {
-			Intent intentMap = new Intent();
-	        intentMap.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level01.GLActivity.class.getName());
-			startActivity(intentMap);
+    		
+    		int iLastLevel = (int)mProgress;
+    		mProgress += ((float)10)/100;
+    		mUiUpdateHandler.sendEmptyMessage(0);
+    		
+			int iId = v.getId();
+			switch(iId) {
+			case R.id.l00_ImageButtonLevel00:
+				//don't do anything
+				break;
+				
+			/*case R.id.l00_ImageButtonLevel01:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level01.GLActivity.class.getName());
+					startActivityForResult(levelIntent, 1);
+				}
+				break;
+			case R.id.l00_ImageButtonLevel02:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level02.GlActivity.class.getName());
+					startActivityForResult(levelIntent, 2);
+				}
+				break;
+			case R.id.l00_ImageButtonLevel03:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level03.GlActivity.class.getName());
+					startActivityForResult(levelIntent, 3);
+				}
+				break;
+			case R.id.l00_ImageButtonLevel04:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level04.GlActivity.class.getName());
+					startActivityForResult(levelIntent, 4);
+				}
+				break;
+			case R.id.l00_ImageButtonLevel05:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level05.GlActivity.class.getName());
+					startActivityForResult(levelIntent, 5);
+				}
+				break;
+			case R.id.l00_ImageButtonLevel06:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level06.GlActivity.class.getName());
+					startActivityForResult(levelIntent, 6);
+				}
+				break;
+			case R.id.l00_ImageButtonLevel07:
+				if(mProgress>=0) {
+					Intent levelIntent = new Intent();
+					levelIntent.setClassName(Constants.PACKAGE_NAME, at.ac.tuwien.cg.cgmd.bifth2010.level07.GlActivity.class.getName());
+					startActivityForResult(levelIntent, 7);
+				}
+				break;
+*/
+			};
+			
 		}
     };
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+    	if(requestCode==Activity.RESULT_OK) {
+    		//get the points the user got in this level
+    		//points must always be in the range 0-100
+    		int iPoints = data.getIntExtra(Constants.RETURN_VALUE_POINTS, 0);
+    		if(iPoints<0) iPoints = 0;
+    		if(iPoints>100) iPoints = 100;
+    		int iLastLevel = (int)mProgress;
+    		mProgress += ((float)iPoints)/100;
+    		int iNewLevel = (int)mProgress;
+    		if (iNewLevel == Constants.NUMBER_OF_LEVELS)
+    		{
+    			//user won
+    			//TODO
+    			//finish game    			
+    		} else if (iNewLevel>iLastLevel) {
+    			//TODO
+    			//add new level
+    		}
+    		
+    		
+    	}
+    }
     
     private OnClickListener mLevelClickListener02 = new OnClickListener(){
 
