a681bc3da3bd74bfe6836252f1a66427a7746642
pontomedon <pontomedon@77361afc-002d-11df-9d03-c52165109085>
2010-03-13 19:10:14 +0000

    Level 42 initial commit mit dummy layouts

---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/LevelActivity.java
@@ -0,0 +1,15 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level42;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+
+public class LevelActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.l42_level);
+	}
+}
