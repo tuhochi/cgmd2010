83b3001daf9da3985d29d93a8385c2b4e4f13b22
pontomedon <pontomedon@77361afc-002d-11df-9d03-c52165109085>
2010-03-13 19:10:14 +0000

    Level 42 initial commit mit dummy layouts

---
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level42/HelpActivity.java
@@ -0,0 +1,17 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level42;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+
+/**
+ * The HelpActivity for test level 42. This activity only demonstrates how a HelpActivity shall be used by each level. 
+ * @author Peter
+ */
+public class HelpActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.l42_help);
+	}
+}
