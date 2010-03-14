56fd5084977e4c00bb686f29b2e4e4e6272059cd
pontomedon <pontomedon@77361afc-002d-11df-9d03-c52165109085>
2010-03-14 13:46:17 +0000

    Level 42 HelpActivity erweitert

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/HelpActivity.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/HelpActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import at.ac.tuwien.cg.cgmd.bifth2010.R;
 
 /**
- * The HelpActivity for test level 42. This activity only demonstrates how a HelpActivity shall be used by each level. 
- * @author Peter
+ * The HelpActivity for test level 42. 
  */
-public class HelpActivity extends Activity {
-
-	public void onCreate(Bundle savedInstanceState) {
+public class HelpActivity extends Activity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.l42_help);
+		// triggers an exception in the framework
+//		setResult(Activity.RESULT_OK);
 	}
 }
