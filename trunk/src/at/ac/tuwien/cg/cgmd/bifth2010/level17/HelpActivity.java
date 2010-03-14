040bab88cf1c9e66215f478cf95c6928ae0e8868
johannes.scharl.at@gmail.com <johannes.scharl.at@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-14 18:43:58 +0000

    + added helpactivity for level 17

---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level17/HelpActivity.java
@@ -0,0 +1,17 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level17;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+
+/**
+ * The HelpActivity for level 17.
+ * @author Johannes Scharl
+ */
+public class HelpActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.l00_help);
+	}
+}
