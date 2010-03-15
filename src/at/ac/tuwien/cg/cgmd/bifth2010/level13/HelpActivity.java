a30ac5538f32100ffe100846b085ec1981456ce5
arthur.oberhauser@gmail.com <arthur.oberhauser@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-15 11:06:46 +0000

    added level 13 help activity

---
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level13/HelpActivity.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level13;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+
+public class HelpActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.l13_help);
+	}
+}
