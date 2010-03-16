727dc19d6703b6e1df58242e24daa551f99f8e42
peter.rautek <peter.rautek@77361afc-002d-11df-9d03-c52165109085>
2010-03-16 19:08:31 +0000

    manually reverted level13

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level13/HelpActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level13/HelpActivity.java
@@ -1,16 +1,16 @@
-package at.ac.tuwien.cg.cgmd.bifth2010.level60;
+package at.ac.tuwien.cg.cgmd.bifth2010.level13;
 
 import android.app.Activity;
 import android.os.Bundle;
 import at.ac.tuwien.cg.cgmd.bifth2010.R;
 
 /**
- * The HelpActivity for level 60. 
- * @author Group 60
+ * The HelpActivity for level 13. 
+ * @author Group 13
  */
 public class HelpActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.l60_help);
+		setContentView(R.layout.l13_help);
 	}
 }
