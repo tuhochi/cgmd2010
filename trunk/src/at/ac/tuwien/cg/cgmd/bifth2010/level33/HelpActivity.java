4595e1e42f6439f3bdef7c490e7986ba764890f3
tuhochi <tuhochi@77361afc-002d-11df-9d03-c52165109085>
2010-03-16 08:16:31 +0000

    Add HelpActivity

---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level33/HelpActivity.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level33;
+
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.l33_help);
+	}
+}
+
+
+
