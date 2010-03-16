7526d049ddf12c51eb428be5c77a9b390813b2e2
dreimalbla@gmail.com <dreimalbla@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-16 12:15:27 +0000

    klassengerüst, opengl test (sich drehendes texturiertes quadrat)

---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level11/Vector2.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level11;
+
+public class Vector2 {
+
+	public float x, y;
+	
+	public void Vector2 () {
+		
+	}
+	
+	public void set(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+}
