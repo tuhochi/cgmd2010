bb19f27200503751291c1fc2ec6ad7664c82f388
robert.hausmair@gmail.com <robert.hausmair@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-16 21:49:38 +0000

    Abgabe Gruppe 70

---
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level70/HelpActivity.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level70;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+
+public class HelpActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.l70_help);
+	}
+}
