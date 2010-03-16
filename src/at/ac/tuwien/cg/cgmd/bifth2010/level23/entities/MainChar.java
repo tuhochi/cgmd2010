2a6ce8b41d8f5934e13c735db0b37d1435e6e1fd
infamoustt@gmail.com <infamoustt@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-16 20:12:03 +0000

    MainChar erweitert (texturen, move)

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/MainChar.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/MainChar.java
@@ -9,6 +9,7 @@ import java.nio.ShortBuffer;
 import javax.microedition.khronos.opengles.GL10;
 
 import at.ac.tuwien.cg.cgmd.bifth2010.level17.Vector2;
+import at.ac.tuwien.cg.cgmd.bifth2010.level17.Vector3;
 
 
 public class MainChar implements SceneEntity {
@@ -16,16 +17,23 @@ public class MainChar implements SceneEntity {
 	private float width;
 	private float height;
 	private Vector2 position;
+	private Vector3 translation;
+	
 	private FloatBuffer vertexBuffer;
 	private ShortBuffer indexBuffer;
-	
+	private FloatBuffer texCoordBuffer;
+	private int textureID = -1;
+		
 	public MainChar()
 	{
 		//create Default MainChar
 		this.height = 200f;
 		this.width = 100f;
 		this.position = new Vector2(200,0);
+		this.translation = new Vector3(0,0,0);
 		createVertexBuffer();
+		createIndexBuffer();
+		createTexCoordBuffer();
 	}
 	
 	public MainChar(float width, float height, Vector2 position)
@@ -33,7 +41,10 @@ public class MainChar implements SceneEntity {
 		this.height = height;
 		this.width = width;
 		this.position = position;
+		this.translation = new Vector3(0,0,0);
 		createVertexBuffer();
+		createIndexBuffer();
+		createTexCoordBuffer();
 	}
 	
 	private void createVertexBuffer()
@@ -65,20 +76,85 @@ public class MainChar implements SceneEntity {
 		vertexBuffer = vertexBBuffer.asFloatBuffer();
 		vertexBuffer.put(vertices);
 		vertexBuffer.position(0);	
-		
+	}
+	
+	private void createIndexBuffer()
+	{
 		short[] indices = { 0, 1, 2, 0, 2, 3 };
 		ByteBuffer indexBBuffer = ByteBuffer.allocateDirect(indices.length * 2);
 		indexBBuffer.order(ByteOrder.nativeOrder());
 		indexBuffer = indexBBuffer.asShortBuffer();
 		indexBuffer.put(indices);
-		indexBuffer.position(0);
+		indexBuffer.position(0);				
+	}
+	
+	private void createTexCoordBuffer()
+	{
+		float[] textureCoordinates = new float[8];
+		//flipping the y-coordinate here
+		//bottom left
+		textureCoordinates[0] = 0.f;
+		textureCoordinates[1] = 1.f;
+		//bottom right
+		textureCoordinates[2] = 1.f;
+		textureCoordinates[3] = 1.f;
+		//top right
+		textureCoordinates[4] = 1.f;
+		textureCoordinates[5] = 0.f;
+		//top left
+		textureCoordinates[6] = 0.f;
+		textureCoordinates[7] = 0.f;
+		
+		ByteBuffer tcbb = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
+		tcbb.order(ByteOrder.nativeOrder());
+		texCoordBuffer = tcbb.asFloatBuffer();
+		texCoordBuffer.put(textureCoordinates);
+		texCoordBuffer.position(0);
+	}
+	
+	public void setTextureID(int texID)
+	{
+		this.textureID = texID;
+	}
+	
+	public void moveLeftRight(float translate)
+	{
+		translation.y += translate;
+	}
+	
+	public void moveUpDown(float translate)
+	{
+		translation.x += translate;
+	}
+	
+	public void moveBackFront(float translate)
+	{
+		translation.z += translate;
+	}	
+	
+	public void resetTranslation()
+	{
+		translation.x = 0f;
+		translation.y = 0f;
+		translation.z = 0f;
 	}
 	
 	@Override
 	public void render() 
 	{
+		glEnable(GL10.GL_TEXTURE_2D);
+		
+		
+		glTranslatef(translation.x, translation.y, translation.z);
+		
 		glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
 		glDrawElements(GL10.GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indexBuffer);
+		
+		if(textureID != -1)
+		{
+			glBindTexture(GL10.GL_TEXTURE_2D, textureID);
+			glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+			glTexCoordPointer(2, GL10.GL_FLOAT, 0, texCoordBuffer);
+		}
 	}
-
 }
