0e11fb6ab274f92fd8e9c47fe958bb9558b96e27
mike.vasiljevs@gmail.com <mike.vasiljevs@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-16 21:44:53 +0000

    *removed warning *added english help text

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level77/LevelActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level77/LevelActivity.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level77;
 
 import android.app.Activity;
-import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
