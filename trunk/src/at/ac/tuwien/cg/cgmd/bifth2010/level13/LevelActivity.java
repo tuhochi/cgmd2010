015550e043b085e814e3a0de9bc18fdf25f05f4a
sebastian.sippl <sebastian.sippl@77361afc-002d-11df-9d03-c52165109085>
2010-03-20 15:14:52 +0000

    - simplified alpha stuff

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level13/LevelActivity.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level13/LevelActivity.java
@@ -13,7 +13,6 @@ public class LevelActivity extends Activity {
 	
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
 		boozyRenderer = new BoozyRenderer(this);
--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level13/Quad.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level13/Quad.java
@@ -19,9 +19,8 @@ public class Quad {
 	private FloatBuffer vertexBuffer;
 	private FloatBuffer textureBuffer;
 	
-	boolean masking = false;
 	
-	private int[] textures = new int[2];
+	private int[] textures = new int[1];
 	
 	private float vertices[] = {
 			                     -1.0f,-1.0f, 0.0f,
@@ -59,30 +58,10 @@ public class Quad {
 	
 	public void draw(GL10 gl){
 		
-		if (masking){
-			gl.glEnable(GL10.GL_BLEND);
-			gl.glDisable(GL10.GL_DEPTH_TEST);
-			gl.glBlendFunc(GL10.GL_DST_COLOR, GL10.GL_ZERO);
-			gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[1]);
-			
-			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-			
-			
-			gl.glFrontFace(GL10.GL_CW);
-			
-			gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-			gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
-			
-			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-			gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length/3);
-			gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-		}else {
-			gl.glDisable(GL10.GL_BLEND);
-			gl.glEnable(GL10.GL_DEPTH_TEST);
-		}
 		
-		gl.glBlendFunc(GL10.GL_ONE, GL10.GL_ONE);
+		gl.glEnable(GL10.GL_BLEND);
+		
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
 		
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
@@ -97,38 +76,29 @@ public class Quad {
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length/3);
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glDisable(GL10.GL_BLEND);
 	}
 	
 	
+
 	
 	public void loadGLTexture(GL10 gl, Context context){
-		this.loadGLTexture(gl, context,false);
-	}
+		InputStream is = context.getResources().openRawResource(R.drawable.l00_rabit_256);
+
+		
 	
-	public void loadGLTexture(GL10 gl, Context context, boolean mask){
-		InputStream is = context.getResources().openRawResource(R.drawable.l13_testimage);
-		InputStream maskStream = null;
 		
-		masking = mask;
-		
-		if (mask)
-			maskStream = context.getResources().openRawResource(R.drawable.l13_testmask);
 		
 		Bitmap bmp = null;
-	    Bitmap maskImage = null;
 		
 		try{
 			bmp = BitmapFactory.decodeStream(is);
-			if (mask)
-				maskImage = BitmapFactory.decodeStream(maskStream);
+		
 		}finally{
 			try{
 				is.close();
 				is = null;
-				if(mask){
-					maskStream.close();
-					maskStream = null;
-				}	
+				
 				
 			}catch(IOException e){
 				//Bad ... some error should be generated here
@@ -138,10 +108,8 @@ public class Quad {
 			
 		}
 		
-		if (mask)
-			gl.glGenTextures(2, textures,0);
-		else
-			gl.glGenTextures(1, textures,0);
+		
+		gl.glGenTextures(1, textures,0);
 		
 			
 		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
@@ -153,21 +121,7 @@ public class Quad {
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
 		
 		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bmp, 0);
-		
-		
-		if(mask){
-			gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[1]);
-			gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
-			gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-		
-			gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
-			gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
-			
-			GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, maskImage, 0);	
-			
-			maskImage.recycle();
-		}
-		
+
 		
 		bmp.recycle();
 		
