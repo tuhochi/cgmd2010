44fa874b477c8a5ed3d48a260273a6c9efa9cb9c
linusine <linusine@77361afc-002d-11df-9d03-c52165109085>
2010-03-21 12:13:14 +0000

    l36 - Paint Beispielprogramm

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level36/LevelActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level36/LevelActivity.java
@@ -1,61 +1,180 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level36;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.EmbossMaskFilter;
+import android.graphics.MaskFilter;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Bundle;
-import android.util.Log;
-import at.ac.tuwien.cg.cgmd.bifth2010.framework.SessionState;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * 
  * @author Gruppe 36
  *
  */
-public class LevelActivity extends Activity{
-	private View scene;
+public class LevelActivity extends Activity {
+	private Paint paint;
+	private MaskFilter mEmboss;
+	private MaskFilter mBlur;
+	private static final int EMBOSS_MENU_ID = Menu.FIRST;
+	private static final int BLUR_MENU_ID = Menu.FIRST + 1;
+	private static final int ERASE_MENU_ID = Menu.FIRST + 2;
+	private static final int SRCATOP_MENU_ID = Menu.FIRST + 3;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Log.v("LevelActivity", "onCreate");
 		super.onCreate(savedInstanceState);
-		scene = new View(this);
-		setContentView(scene);
+		setContentView(new MyView(this));
+		paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setDither(true);
+		paint.setColor(0xFFFF0000);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeJoin(Paint.Join.ROUND);
+		paint.setStrokeCap(Paint.Cap.ROUND);
+		paint.setStrokeWidth(10);
+		mEmboss = new EmbossMaskFilter(new float[] { 1, 1, 1 }, 0.4f, 6, 3.5f);
+		mBlur = new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL);
 	}
 	
-	/**
-	 * 
-	 * @param score
-	 */
-	private void finish(int score) {
-		Log.v("LevelActivity", "finish");
-		//the SessionState is a convenience class to set a result
-		SessionState s = new SessionState();
-		//we set the progress the user has made (must be between 0-100)
-		s.setProgress(score);
-		//we call the activity's setResult method 
-		setResult(Activity.RESULT_OK, s.asIntent());
-		//we finish this activity
-		finish();
-	}
-	
-	/**
-	 * Resume the scene
-	 */
 	@Override
-	protected void onResume() {
-		super.onResume();
-		if (scene != null) {
-			scene.onResume();
-		}
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, EMBOSS_MENU_ID, 0, "Emboss").setShortcut('4', 's');
+		menu.add(0, BLUR_MENU_ID, 0, "Blur").setShortcut('5', 'z');
+		menu.add(0, ERASE_MENU_ID, 0, "Erase").setShortcut('5', 'z');
+		menu.add(0, SRCATOP_MENU_ID, 0, "SrcATop").setShortcut('5', 'z');
+		return true;
 	}
 
-	/**
-	 * Pause the scene
-	 */
 	@Override
-	protected void onPause() {
-		super.onPause();
-		if (scene != null) {
-			scene.onPause();
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		paint.setXfermode(null);
+		paint.setAlpha(0xFF);
+
+		switch (item.getItemId()) {
+		case EMBOSS_MENU_ID:
+			if (paint.getMaskFilter() != mEmboss) {
+				paint.setMaskFilter(mEmboss);
+			} else {
+				paint.setMaskFilter(null);
+			}
+			return true;
+		case BLUR_MENU_ID:
+			if (paint.getMaskFilter() != mBlur) {
+				paint.setMaskFilter(mBlur);
+			} else {
+				paint.setMaskFilter(null);
+			}
+			return true;
+		case ERASE_MENU_ID:
+			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+			return true;
+		case SRCATOP_MENU_ID:
+			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+			paint.setAlpha(0x80);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	
+	
+	
+	
+	
+	private class MyView extends View {
+		private Bitmap mBitmap;
+		private Canvas mCanvas;
+		private Path mPath;
+		private Paint mBitmapPaint;
+		private float mX, mY;
+		private static final float TOUCH_TOLERANCE = 4;
+		
+		public MyView(Context c) {
+			super(c);
+			mBitmap = Bitmap.createBitmap(320, 480, Bitmap.Config.ARGB_8888);
+			mCanvas = new Canvas(mBitmap);
+			mPath = new Path();
+			mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+		}
+
+		@Override
+		protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+			super.onSizeChanged(w, h, oldw, oldh);
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			canvas.drawColor(Color.WHITE);
+			// das bereits gezeichnete
+			canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+			// was jetzt gezeichnet wird
+			canvas.drawPath(mPath, paint);
+		}
+
+		private void touch_start(float x, float y) {
+			mPath.reset();
+			mPath.moveTo(x, y);
+			mX = x;
+			mY = y;
+		}
+
+		private void touch_move(float x, float y) {
+			float dx = Math.abs(x - mX);
+			float dy = Math.abs(y - mY);
+			if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+				mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+				mX = x;
+				mY = y;
+			}
+		}
+
+		private void touch_up() {
+			mPath.lineTo(mX, mY);
+			// commit the path to our offscreen
+			mCanvas.drawPath(mPath, paint);
+			// kill this so we don't double draw
+			mPath.reset();
+		}
+
+		@Override
+		public boolean onTouchEvent(MotionEvent event) {
+			float x = event.getX();
+			float y = event.getY();
+
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				touch_start(x, y);
+				invalidate();
+				break;
+			case MotionEvent.ACTION_MOVE:
+				touch_move(x, y);
+				invalidate();
+				break;
+			case MotionEvent.ACTION_UP:
+				touch_up();
+				invalidate();
+				break;
+			}
+			return true;
 		}
 	}
 }
