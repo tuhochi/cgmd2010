fd974102c07657e8e0e7bef96849f43a716ce46c
matthiasmaschek <matthiasmaschek@77361afc-002d-11df-9d03-c52165109085>
2010-03-27 15:00:09 +0000

    LVL17: CHG Fixed House generation, Added Fog and fadeout

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level17/Level.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level17/Level.java
@@ -1,5 +1,10 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level17;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.microedition.khronos.opengles.GL10;
+
 import at.ac.tuwien.cg.cgmd.bifth2010.level17.math.MatrixTrackingGL;
 import at.ac.tuwien.cg.cgmd.bifth2010.level17.math.Vector3;
 import at.ac.tuwien.cg.cgmd.bifth2010.level17.renderables.HouseModel;
@@ -7,8 +12,12 @@ import at.ac.tuwien.cg.cgmd.bifth2010.level17.renderables.HouseModel;
 
 public class Level {
 
-	private HouseModel[] mHouses = new HouseModel[5];
-	private Vector3 mPosition = new Vector3(5.0f, 0, 0);
+	private HouseModel[] mHouseModels = new HouseModel[5];
+	private Vector3 mPosition = new Vector3(0, 0, 0);
+	private List<HouseInfo> mHouses = new ArrayList<HouseInfo>();
+	private List<HouseInfo> mFadeHouses = new ArrayList<HouseInfo>();
+	private Vector3 mSpeed = new Vector3(0, 20.0f, 0);
+	private float mBlockSize = 5.0f;
 	
 	/**
 	 * Level Class for Rendering Houses and other Objects.
@@ -16,7 +25,25 @@ public class Level {
 	public Level()
 	{
 		for(int i = 0; i < 5; i++)
-			mHouses[i] = new HouseModel(5.0f, 5.0f * ((float)i + 1.0f), 5.0f);
+			mHouseModels[i] = new HouseModel(mBlockSize, mBlockSize * ((float)i + 1.0f), mBlockSize);
+		
+		HouseInfo house = new HouseInfo();
+		house.setPosition(new Vector3(mBlockSize,-30.0f,0));
+		house.setHouseSize(1);
+		mHouses.add(house);
+		house = new HouseInfo();
+		house.setPosition(new Vector3(-mBlockSize,-32.5f,0));
+		house.setHouseSize(2);
+		mHouses.add(house);
+		house = new HouseInfo();
+		house.setPosition(new Vector3(0.0f,-35.0f,mBlockSize));
+		house.setHouseSize(3);
+		mHouses.add(house);
+		house = new HouseInfo();
+		house.setPosition(new Vector3(0.0f,-37.5f,-mBlockSize));
+		house.setHouseSize(4);
+		mHouses.add(house);
+		
 	}
 	
 	/**
@@ -27,7 +54,29 @@ public class Level {
 	{
 		gl.glPushMatrix();
 		gl.glTranslatef(mPosition);
-		mHouses[0].draw(gl);
+		for (HouseInfo house : mHouses) {
+			gl.glPushMatrix();
+			gl.glTranslatef(house.getPosition());
+			mHouseModels[house.getHouseSize()].draw(gl);
+			gl.glPopMatrix();
+		}
+
+    	gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+    	gl.glEnable(GL10.GL_BLEND);
+    	gl.glDisable(GL10.GL_DEPTH_TEST);
+    	
+		for (HouseInfo house : mFadeHouses) {
+			gl.glPushMatrix();
+			gl.glTranslatef(house.getPosition());
+			float yPos = house.getPosition().y + mPosition.y  + (house.getHouseSize() * mBlockSize / 2.0f);
+        	gl.glColor4f(1.0f,1.0f,1.0f, 1.0f - (yPos / 10.0f));
+			mHouseModels[house.getHouseSize()].draw(gl);	
+			gl.glPopMatrix();
+		}
+    	gl.glColor4f(1.0f,1.0f,1.0f, 1.0f);
+    	gl.glDisable(GL10.GL_BLEND);
+    	gl.glEnable(GL10.GL_DEPTH_TEST);
+    	
 		gl.glPopMatrix();
 	}
 	
@@ -37,6 +86,33 @@ public class Level {
 	 */
 	public void update(float elapsedSeconds)
 	{
-		mPosition = Vector3.add(mPosition, Vector3.mult(new Vector3(0,1.0f,0), elapsedSeconds));	
+		mPosition = Vector3.add(mPosition, Vector3.mult(mSpeed, elapsedSeconds));	
+		
+		List<HouseInfo> remove = new ArrayList<HouseInfo>();
+		List<HouseInfo> add = new ArrayList<HouseInfo>();
+		
+		for (HouseInfo house : mHouses) {
+			if(house.getPosition().y + mPosition.y  + (house.getHouseSize() * mBlockSize / 2.0f)> 0)
+			{
+				remove.add(house);
+				HouseInfo newHouse = new HouseInfo();
+				newHouse.setHouseSize(house.getHouseSize());
+				newHouse.setPosition(new Vector3(house.getPosition().x, -mPosition.y - 50.0f, house.getPosition().z));
+				add.add(newHouse);
+			}
+		}
+		
+		mHouses.removeAll(remove);
+		mFadeHouses.addAll(remove);
+		mHouses.addAll(add);
+		remove.clear();
+		add.clear();
+		
+		for (HouseInfo house : mFadeHouses) {
+			if(house.getPosition().y + mPosition.y  + (house.getHouseSize() * mBlockSize / 2.0f) > 10.0f)
+				remove.add(house);
+		}
+		mFadeHouses.removeAll(remove);
+		
 	}
 }
