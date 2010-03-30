cb168d84299c63858c6fa1142cce87d5097c0141
pontomedon <pontomedon@77361afc-002d-11df-9d03-c52165109085>
2010-03-30 17:00:49 +0000

    l42 - format change

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/math/Sphere.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/math/Sphere.java
@@ -1,29 +1,27 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level42.math;
 
-import android.util.Log;
-import at.ac.tuwien.cg.cgmd.bifth2010.level42.LevelActivity;
+import java.util.ArrayList;
 
 public class Sphere
 {
 	public final Vector3 center;
 	public final Vector3 radius;
 	
-	private Vector3 temp;
+	private final Vector3 temp;
 	
 	public Sphere()
 	{
-		this(new Vector3(0,0,0), new Vector3(0,0,0));
+		this(new Vector3(0,0,0), 0);
 	}
 	
 	public Sphere(Sphere other)
 	{
-		center = new Vector3(other.center);
-		radius = new Vector3(other.radius);
+		this(new Vector3(other.center), new Vector3(other.radius));
 	}
 	
 	public Sphere(Vector3 center, float radius)
 	{
-		this(center,new Vector3(1,0,0).multiply(radius));
+		this(center, new Vector3(radius,0,0));
 	}
 	
 	public Sphere(Vector3 center, Vector3 radius)
@@ -32,12 +30,12 @@ public class Sphere
 		this.radius = radius;
 		temp = new Vector3();
 	}
-	
+
 	public float getRadius()
 	{
 		return radius.length();
 	}
-
+	
 	public void include(Sphere boundingSphere)
 	{
 		// temp = vector between centers
@@ -51,25 +49,92 @@ public class Sphere
 		if(distanceBetweenCenters != 0)	// spheres have different centers
 		{
 			// radius of the resulting sphere
-			float newRadius = (distanceBetweenCenters + boundingSphere.getRadius() + getRadius())/2.0f;
+			float newRadius = (distanceBetweenCenters + boundingSphere.radius.length() + radius.length())/2.0f;
 			
 			// temp = radius of this sphere in direction of the two centers
 			temp.normalize();
-			temp.multiply(getRadius());
+			temp.multiply(radius);
 			// center = point on hull of this sphere on the line containing both centers
 			center.subtract(temp);
 			
 			// temp = radius of the resulting sphere
 			temp.normalize();
 			temp.multiply(newRadius);
-			radius.copy(temp);
 			
 			// center = center of the resulting sphere
-			center.add(radius);
+			center.add(temp);
+			radius.copy(temp);
 		}
 		else	// both spheres share the same center
 		{
-			radius.normalize().multiply(Math.max(getRadius(), boundingSphere.getRadius()));
+			radius.x = Math.max(radius.length(), boundingSphere.radius.length());
+			radius.y = 0;
+			radius.z = 0;
 		}
+	}
+	
+	public boolean isPointInside(Vector3 point)
+	{
+		temp.copy(point);
+		return temp.subtract(center).length() <= radius.length();
+	}
+	
+	public void setPointSet(ArrayList<Vector3> vertices)
+	{
+		Vector3 centerBB = new Vector3();
+		float radiusBB = calcSphereFromBBox(vertices, centerBB);
+		
+		Vector3 centerC = new Vector3();
+		float radiusC = calcSphereFromCentroid(vertices, centerC);
+		
+		if(radiusBB < radiusC)
+		{
+			center.copy(centerBB);
+			radius.x = radiusBB;
+		}
+		else
+		{
+			center.copy(centerC);
+			radius.x = radiusC;
+		}
+		radius.y = 0;
+		radius.z = 0;
+	}
+	
+	private float calcSphereFromBBox(ArrayList<Vector3> vertices, Vector3 center)
+	{
+		AxisAlignedBox3 bbox = new AxisAlignedBox3();
+		for(Vector3 v : vertices)
+			bbox.include(v);
+		
+		center.copy(bbox.center());
+		
+		float r = 0;
+		for(Vector3 v : vertices)
+		{
+			float temp_r = Vector3.subtract(v, center).length();
+			if(temp_r > r)
+				r = temp_r;
+		}
+		return r;
+	}
+	
+	private float calcSphereFromCentroid(ArrayList<Vector3> vertices, Vector3 center)
+	{
+		Vector3 centroid = new Vector3();
+		for(Vector3 v : vertices)
+			centroid.add(v);
+		centroid.divide(vertices.size());
+		
+		center.copy(centroid);
+		
+		float r = 0;
+		for(Vector3 v : vertices)
+		{
+			float temp_r = Vector3.subtract(v, center).length();
+			if(temp_r > r)
+				r = temp_r;
+		}
+		return r;
 	}
 }
