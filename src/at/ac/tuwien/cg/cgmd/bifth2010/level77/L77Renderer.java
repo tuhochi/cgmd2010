b2500317cbf7497b5cbbb3fca145697dd37dcf84
mike.vasiljevs@gmail.com <mike.vasiljevs@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-03-31 13:01:25 +0000

    added rendering of the coin

---
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level77/L77Renderer.java
@@ -0,0 +1,114 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level77;
+
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+import android.opengl.GLSurfaceView.Renderer;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
+import at.ac.tuwien.cg.cgmd.bifth2010.framework.Cylinder;
+import at.ac.tuwien.cg.cgmd.bifth2010.framework.Rectangle;
+
+//one of the bloody things needed for getResources()
+import android.content.Context;
+
+
+public class L77Renderer implements Renderer {
+	private float mAngle;
+	private Cylinder mCube;
+    private boolean mTranslucentBackground;
+    private Context mContext;
+
+    public L77Renderer(boolean useTranslucentBackground, Context aContext) {
+        mTranslucentBackground = useTranslucentBackground;
+        mContext = aContext;
+    }
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO Adapt CubeRend to perosnal stuff
+		 /*
+         * Usually, the first thing one might want to do is to clear
+         * the screen. The most efficient way of doing this is to use
+         * glClear().
+         */
+
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+        /*
+         * Now we're ready to draw some 3D objects
+         */
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0, 0, -3.0f);
+        gl.glRotatef(mAngle,        0, 1, 0);
+        gl.glRotatef(mAngle*0.25f,  1, 0, 0);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+        mCube.draw(gl);
+
+        gl.glRotatef(mAngle*2.0f, 0, 1, 1);
+        gl.glTranslatef(0.5f, 0.5f, 0.5f);
+
+        mCube.draw(gl);
+
+        mAngle += 1.2f;
+
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Change default
+        gl.glViewport(0, 0, width, height);
+
+        /*
+         * Set our projection matrix. This doesn't have to be done
+         * each time we draw, but usually a new projection needs to
+         * be set when the viewport is resized.
+         */
+
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Change default stuff
+        /*
+         * By default, OpenGL enables features that improve quality
+         * but reduce performance. One might want to tweak that
+         * especially on software renderer.
+         */
+        gl.glDisable(GL10.GL_DITHER);
+
+        /*
+         * Some one-time OpenGL initialization can be made here
+         * probably based on features of this particular context
+         */
+         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                 GL10.GL_FASTEST);
+
+         if (mTranslucentBackground) {
+             gl.glClearColor(0,0,0,0);
+         } else {
+             gl.glClearColor(1,1,1,1);
+         }
+         gl.glEnable(GL10.GL_CULL_FACE);
+         gl.glShadeModel(GL10.GL_SMOOTH);
+         gl.glEnable(GL10.GL_DEPTH_TEST);
+         
+        mCube = new Cylinder(0.5f, 0.10f, (short) 10);
+ 		mCube.setTexture(gl, mContext.getResources(), R.drawable.l00_coin);
+ 		mCube.setColor(0.85f, 0.68f, 0.22f, 1.f);
+	}
+
+}
--- src/at/ac/tuwien/cg/cgmd/bifth2010/level77/LevelActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level77/LevelActivity.java
@@ -1,13 +1,17 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level77;
 
 import android.app.Activity;
+
 //import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import at.ac.tuwien.cg.cgmd.bifth2010.R;
 import at.ac.tuwien.cg.cgmd.bifth2010.framework.SessionState;
+
+import android.content.Context;
 
 /**
  * This activity demonstrates the basic interaction with the framework. When the Finish button is pressed the a result is set and the activity is finished. 
@@ -16,30 +20,58 @@ import at.ac.tuwien.cg.cgmd.bifth2010.framework.SessionState;
  */
 public class LevelActivity extends Activity
 {
+	private GLSurfaceView mGLSurfaceView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		//set a layout
-		setContentView(R.layout.l77_level);
-		//get the button specified in the layout
-		Button buttonFinish = (Button) findViewById(R.id.l77_ButtonFinish);
-		//set a onClickListener to react to the user's click
-		buttonFinish.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				//the SessionState is a convenience class to set a result
-				SessionState s = new SessionState();
-				//we set the progress the user has made (must be between 0-100)
-				s.setProgress(1);
-				//we call the activity's setResult method 
-				setResult(Activity.RESULT_OK, s.asIntent());
-				//we finish this activity
-				LevelActivity.this.finish();
-			}
-			
-		});
+		
+		setContentView( getView() );
+		
+//		//get the button specified in the layout
+//		Button buttonFinish = (Button) findViewById(R.id.l77_ButtonFinish);
+//		//set a onClickListener to react to the user's click
+//		buttonFinish.setOnClickListener(new OnClickListener(){
+//			@Override
+//			public void onClick(View v) {
+//				//the SessionState is a convenience class to set a result
+//				SessionState s = new SessionState();
+//				//we set the progress the user has made (must be between 0-100)
+//				s.setProgress(1);
+//				//we call the activity's setResult method 
+//				setResult(Activity.RESULT_OK, s.asIntent());
+//				//we finish this activity
+//				LevelActivity.this.finish();
+//			}
+//			
+//		});
+//		
+		
 	}
 	
+	private GLSurfaceView getView()
+	{
+	//	return R.layout.l77_level;
+		// gl stuff
+        // Create our Preview view and set it as the content of our
+        // Activity
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setRenderer(new L77Renderer( false, mGLSurfaceView.getContext() ));
+        
+        return mGLSurfaceView;
+	}
+	
+    @Override
+	protected void onPause() {
+		super.onPause();
+		mGLSurfaceView.onPause();		
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mGLSurfaceView.onResume();
+	}
 }
