e9010a7219793d9baf0cfe45ef2bc69b59078d0f
grange.tuwien <grange.tuwien@77361afc-002d-11df-9d03-c52165109085>
2010-04-06 16:19:42 +0000

    GeometryManager implementiert, MainChar und Background dementsprechend adaptiert, Warnings entfernt

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/MainChar.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/MainChar.java
@@ -1,32 +1,36 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level23.entities;
-import static android.opengl.GLES10.*;
+import static android.opengl.GLES10.GL_FLOAT;
+import static android.opengl.GLES10.GL_TRIANGLE_STRIP;
+import static android.opengl.GLES10.glBindTexture;
+import static android.opengl.GLES10.glDrawArrays;
+import static android.opengl.GLES10.glPopMatrix;
+import static android.opengl.GLES10.glPushMatrix;
+import static android.opengl.GLES10.glTexCoordPointer;
+import static android.opengl.GLES10.glTranslatef;
+import static android.opengl.GLES10.glVertexPointer;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
-import android.hardware.Camera.Size;
-import android.opengl.GLES10;
 import android.opengl.GLES11;
 import at.ac.tuwien.cg.cgmd.bifth2010.level17.math.Vector2;
 import at.ac.tuwien.cg.cgmd.bifth2010.level17.math.Vector3;
 import at.ac.tuwien.cg.cgmd.bifth2010.level23.render.RenderView;
-import at.ac.tuwien.cg.cgmd.bifth2010.level42.scene.Scene;
-import at.ac.tuwien.cg.cgmd.bifth2010.level42.util.Config;
+import at.ac.tuwien.cg.cgmd.bifth2010.level23.util.GeometryManager;
 
 
 public class MainChar implements SceneEntity {
 
+	
+	private static final long serialVersionUID = -8661277209785862751L;
 	private float width;
 	private float height;
 	private Vector2 position;
 	private Vector3 translation;
+	private GeometryManager geometryManager; 
 	
 	private FloatBuffer vertexBuffer;
-	private ShortBuffer indexBuffer;
 	private FloatBuffer texCoordBuffer;
 	private int vboId; 
 	private boolean vbo = true;
@@ -47,11 +51,7 @@ public class MainChar implements SceneEntity {
 		this.width = 100f;
 		this.position = new Vector2(200,0);
 		this.translation = new Vector3(0,0,0);
-		createVertexBuffer();
-		createIndexBuffer();
-		createTexCoordBuffer();
-//		if (vbo)
-			setupVBO();
+		preprocess();
 	}
 	
 	public MainChar(float width, float height, Vector2 position)
@@ -60,30 +60,19 @@ public class MainChar implements SceneEntity {
 		this.width = width;
 		this.position = position;
 		this.translation = new Vector3(0,0,0);
-		createVertexBuffer();
-		createIndexBuffer();
-		createTexCoordBuffer();
-//		if(vbo)
-			setupVBO();
+		preprocess(); 
 	}
 	
-	private void setupVBO() {
+	private void preprocess() {
 		
-		int[] ids = new int[1];
-		GLES11.glGenBuffers(1, ids, 0);
-		vboId = ids[0];
+		geometryManager = GeometryManager.getInstance(); 
+		vertexBuffer = geometryManager.createVertexBufferQuad(width, height);
+		texCoordBuffer = geometryManager.createTexCoordBufferQuad();
+		vboId = geometryManager.createVBO(vertexBuffer, texCoordBuffer);
 		
-		GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, vboId);
-		int size = (vertexBuffer.capacity()+texCoordBuffer.capacity())* 4; //4*3 Vertices, 4*2 Texcoords, 4 Byte je Float
-		GLES11.glBufferData(GLES11.GL_ARRAY_BUFFER, size, null, GLES11.GL_STATIC_DRAW);
-		
-		GLES11.glBufferSubData(GLES11.GL_ARRAY_BUFFER, 0, vertexBuffer.capacity()*4, vertexBuffer);
-		GLES11.glBufferSubData(GLES11.GL_ARRAY_BUFFER, vertexBuffer.capacity()*4, texCoordBuffer.capacity()*4, texCoordBuffer);
-		GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, 0);
-		
-		//GLES11.glBufferSubData(GLES11.GL_ELEMENT_ARRAY_BUFFER, (vertexBuffer.capacity() + texCoordBuffer.capacity())*4, indexBuffer.capacity()*2, indexBuffer);
+	}
 	
-}
+
 	/** 
 	 * @return the moveDirection
 	 */
@@ -113,71 +102,7 @@ public class MainChar implements SceneEntity {
 	public float getHeight() {
 		return this.height; 
 	}
-	private void createVertexBuffer()
-	{
-		float x = position.x;
-		float y = position.y;
-		float[] vertices = new float[12];
-		
-		//origin = 0 0
-		//bottom left
-		vertices[0] = 0f + x;
-		vertices[1] = 0f + y;
-		vertices[2] = 0f;
-		//bottom right
-		vertices[3] = width + x;
-		vertices[4] = 0f + y;
-		vertices[5] = 0f;
-		//top left
-		vertices[6] = 0f + x;
-		vertices[7]= height + y;
-		vertices[8]= 0f;
-		//top right
-		vertices[9] = width + x;
-		vertices[10] = height + y;
-		vertices[11] = 0f;
-				
-		ByteBuffer vertexBBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
-		vertexBBuffer.order(ByteOrder.nativeOrder());
-		vertexBuffer = vertexBBuffer.asFloatBuffer();
-		vertexBuffer.put(vertices);
-		vertexBuffer.position(0);	
-	}
 	
-	private void createIndexBuffer()
-	{
-		short[] indices = { 0, 1, 2, 3 };
-		ByteBuffer indexBBuffer = ByteBuffer.allocateDirect(indices.length * 2);
-		indexBBuffer.order(ByteOrder.nativeOrder());
-		indexBuffer = indexBBuffer.asShortBuffer();
-		indexBuffer.put(indices);
-		indexBuffer.position(0);				
-	}
-	
-	private void createTexCoordBuffer()
-	{
-		float[] textureCoordinates = new float[8];
-		//flipping the y-coordinate here
-		//bottom left
-		textureCoordinates[0] = 0.f;
-		textureCoordinates[1] = 1.f;
-		//bottom right
-		textureCoordinates[2] = 1.f;
-		textureCoordinates[3] = 1.f;
-		//top left
-		textureCoordinates[4] = 0.f;
-		textureCoordinates[5] = 0.f;
-		//top right
-		textureCoordinates[6] = 1.f;
-		textureCoordinates[7] = 0.f;
-				
-		ByteBuffer tcbb = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
-		tcbb.order(ByteOrder.nativeOrder());
-		texCoordBuffer = tcbb.asFloatBuffer();
-		texCoordBuffer.put(textureCoordinates);
-		texCoordBuffer.position(0);
-		
-	}
 	
 	public void setTextureID(int texID)
 	{
@@ -261,10 +186,10 @@ public class MainChar implements SceneEntity {
 			}		
 		
 			glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-			glDrawElements(GL10.GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, indexBuffer);
+			glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
 		
 		} else {
-		
+	
 			GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, vboId);
 			
 			if (textureID != -1) {
--- src/at/ac/tuwien/cg/cgmd/bifth2010/level23/render/RenderView.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level23/render/RenderView.java
@@ -7,6 +7,7 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.opengl.GLES10;
 import android.opengl.GLSurfaceView;
 import android.view.Display;
 import android.view.KeyEvent;
@@ -344,8 +345,8 @@ public class RenderView extends GLSurfaceView implements GLSurfaceView.Renderer 
 	{
 		gl.glDepthFunc(GL10.GL_LEQUAL);
 		gl.glEnable(GL10.GL_TEXTURE_2D);
-		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		gl.glEnableClientState(GLES10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GLES10.GL_TEXTURE_COORD_ARRAY);
 		gl.glEnable(GL10.GL_BLEND);
 	    gl.glBlendFunc(GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA);
 	    gl.glShadeModel(GL10.GL_FLAT);
