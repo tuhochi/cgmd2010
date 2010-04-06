d091e4635596ab0800af20f36b80d4c99fe9f254
grange.tuwien <grange.tuwien@77361afc-002d-11df-9d03-c52165109085>
2010-04-06 16:19:42 +0000

    GeometryManager implementiert, MainChar und Background dementsprechend adaptiert, Warnings entfernt

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/Background.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/Background.java
@@ -1,122 +1,54 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level23.entities;
-import static android.opengl.GLES10.*;
+import static android.opengl.GLES10.glBindTexture;
+import static android.opengl.GLES10.glPopMatrix;
+import static android.opengl.GLES10.glPushMatrix;
+import static android.opengl.GLES10.glTexCoordPointer;
+import static android.opengl.GLES10.glTranslatef;
+import static android.opengl.GLES10.glVertexPointer;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
+
 import javax.microedition.khronos.opengles.GL10;
 
+import android.opengl.GLES10;
 import at.ac.tuwien.cg.cgmd.bifth2010.level23.render.RenderView;
+import at.ac.tuwien.cg.cgmd.bifth2010.level23.util.GeometryManager;
 
 public class Background implements SceneEntity 
 {
 
-	private FloatBuffer vertexBuffer1;
-	private ShortBuffer indexBuffer;
+	private static final long serialVersionUID = -8706705496517584380L;
+	private FloatBuffer vertexBuffer;
 	private int textureID;
 	private FloatBuffer[] texCoordBuffer;
+	private final float[] texCoords = {0.f, 0.5f, 1.f, 0.5f, 0.f, 0.f, 1.f, 0.f, 0.f, 1.f, 1.f, 1.f, 0.f, 0.5f, 1.f, 0.5f}; 
 	private float scrollSpeed = 0.01f;
 	private float positionY;
 	private RenderView renderView; 
-	
+	private GeometryManager geometryManager; 
 	//hack only needed now for only 2 textures
 	private boolean switchTexture = false;
 	
 	public Background()
 	{
 		renderView = RenderView.getInstance();	
-		texCoordBuffer = new FloatBuffer[2];
-		generateTiles();
-		createIndexBuffer();
-		createTexCoordBuffer();
+		//texCoordBuffer = new FloatBuffer[2];
+		preprocess();
 		
 	}
 	
-	private void generateTiles()
-	{
+	private void preprocess() {
 		
-		float[] vertices = new float[12];
-		float tb = renderView.getTopBounds();
-		float rb = renderView.getRightBounds(); 
-		//origin = 0 0
-		//bottom left
-		vertices[0] = 0f;
-		vertices[1] = 0f;
-		vertices[2] = 0f;
-		//bottom right
-		vertices[3] = rb;
-		vertices[4] = 0f;
-		vertices[5] = 0f;
-		//top left
-		vertices[6] = 0f;
-		vertices[7]= tb;
-		vertices[8]= 0f;
-		//top right
-		vertices[9] = rb;
-		vertices[10] = tb;
-		vertices[11] = 0f;
-				
-		ByteBuffer vertexBBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
-		vertexBBuffer.order(ByteOrder.nativeOrder());
-		vertexBuffer1 = vertexBBuffer.asFloatBuffer();
-		vertexBuffer1.put(vertices);
-		vertexBuffer1.position(0);
+		geometryManager = GeometryManager.getInstance(); 
+		texCoordBuffer = geometryManager.createTexCoordBufferQuad(texCoords);
+		vertexBuffer = geometryManager.createVertexBufferQuad(renderView.getRightBounds(), renderView.getTopBounds());
+		//texCoordBuffer = geometryManager.createTexCoordBufferQuad(texCoords);
+		//vboId = geometryManager.createVBO(vertexBuffer1, texCoordBuffer);
+		
 	}
 	
-	private void createIndexBuffer()
-	{
-		short[] indices = { 0, 1, 2, 3 };
-		ByteBuffer indexBBuffer = ByteBuffer.allocateDirect(indices.length * 2);
-		indexBBuffer.order(ByteOrder.nativeOrder());
-		indexBuffer = indexBBuffer.asShortBuffer();
-		indexBuffer.put(indices);
-		indexBuffer.position(0);				
-	}
 	
-	private void createTexCoordBuffer()
-	{
-		float[] textureCoordinates = new float[8];
-		//flipping the y-coordinate here
-		//bottom left
-		textureCoordinates[0] = 0.f;
-		textureCoordinates[1] = 0.5f;
-		//bottom right
-		textureCoordinates[2] = 1.f;
-		textureCoordinates[3] = 0.5f;
-		//top left
-		textureCoordinates[4] = 0.f;
-		textureCoordinates[5] = 0.f;
-		//top right
-		textureCoordinates[6] = 1.f;
-		textureCoordinates[7] = 0.f;
-				
-		ByteBuffer tcbb = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
-		tcbb.order(ByteOrder.nativeOrder());
-		texCoordBuffer[0] = tcbb.asFloatBuffer();
-		texCoordBuffer[0].put(textureCoordinates);
-		texCoordBuffer[0].position(0);
-		
-		//flipping the y-coordinate here
-		//bottom left
-		textureCoordinates[0] = 0.f;
-		textureCoordinates[1] = 1.f;
-		//bottom right
-		textureCoordinates[2] = 1.f;
-		textureCoordinates[3] = 1.f;
-		//top left                   
-		textureCoordinates[4] = 0.f;
-		textureCoordinates[5] = 0.5f;
-		//top right
-		textureCoordinates[6] = 1.f;
-		textureCoordinates[7] = 0.5f;
-				
-		ByteBuffer tcbb2 = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
-		tcbb2.order(ByteOrder.nativeOrder());
-		texCoordBuffer[1] = tcbb2.asFloatBuffer();
-		texCoordBuffer[1].put(textureCoordinates);
-		texCoordBuffer[1].position(0);
-	}
+
 	
 	public void setTextureID(int texID)
 	{
@@ -143,13 +75,14 @@ public class Background implements SceneEntity
 		//render first BG quad
 		glBindTexture(GL10.GL_TEXTURE_2D, textureID);
 		glTexCoordPointer(2, GL10.GL_FLOAT, 0, texCoordBuffer[switchTexture?1:0]);
-		glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer1);
-		glDrawElements(GL10.GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, indexBuffer);
+		glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+		GLES10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4); 
+		
 		
 		//render second BG quad
 		glTranslatef(0,renderView.getTopBounds(),0);
 		glTexCoordPointer(2, GL10.GL_FLOAT, 0, texCoordBuffer[switchTexture?0:1]);
-		glDrawElements(GL10.GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, indexBuffer);
+		GLES10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
 		
 		glPopMatrix();
 	}
---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level23/util/GeometryManager.java
@@ -0,0 +1,141 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level23.util;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import android.opengl.GLES11;
+
+public class GeometryManager {
+
+	private static GeometryManager geometryManager; 
+	
+	
+	public GeometryManager() {
+		geometryManager = this; 
+	}
+	
+	public FloatBuffer createVertexBufferQuad(float width, float height)
+	{
+		FloatBuffer buffer;
+		
+		
+		float[] vertices = new float[12];
+		
+		//origin = 0 0
+		//bottom left
+		vertices[0] = 0f;
+		vertices[1] = 0f;
+		vertices[2] = 0f;
+		//bottom right
+		vertices[3] = width;
+		vertices[4] = 0f;
+		vertices[5] = 0f;
+		//top left
+		vertices[6] = 0f;
+		vertices[7]= height;
+		vertices[8]= 0f;
+		//top right
+		vertices[9] = width;
+		vertices[10] = height;
+		vertices[11] = 0f;
+				
+		ByteBuffer vertexBBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+		vertexBBuffer.order(ByteOrder.nativeOrder());
+		buffer = vertexBBuffer.asFloatBuffer();
+		buffer.put(vertices);
+		buffer.position(0);	
+		
+		return buffer;
+	}
+	
+	
+	public FloatBuffer createTexCoordBufferQuad()
+	{
+		FloatBuffer texCoordBuffer; 
+		
+		float[] textureCoordinates = new float[8];
+		//flipping the y-coordinate here
+		//bottom left
+		textureCoordinates[0] = 0.f;
+		textureCoordinates[1] = 1.f;
+		//bottom right
+		textureCoordinates[2] = 1.f;
+		textureCoordinates[3] = 1.f;
+		//top left
+		textureCoordinates[4] = 0.f;
+		textureCoordinates[5] = 0.f;
+		//top right
+		textureCoordinates[6] = 1.f;
+		textureCoordinates[7] = 0.f;
+				
+		ByteBuffer tcbb = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
+		tcbb.order(ByteOrder.nativeOrder());
+		texCoordBuffer = tcbb.asFloatBuffer();
+		texCoordBuffer.put(textureCoordinates);
+		texCoordBuffer.position(0);
+		
+		return texCoordBuffer; 
+		
+	}
+	
+	public FloatBuffer[] createTexCoordBufferQuad(float[] coordinates)
+	{
+
+		FloatBuffer[] texCoordBuffer = new FloatBuffer[coordinates.length / 8]; 
+		float[] textureCoordinates = new float[8];
+		
+		for (int i=0; i< coordinates.length; i=i+8) {
+			//bottom left
+			textureCoordinates[0] = coordinates[i];
+			textureCoordinates[1] = coordinates[i+1];
+			//bottom right
+			textureCoordinates[2] = coordinates[i+2];
+			textureCoordinates[3] = coordinates[i+3];
+			//top left
+			textureCoordinates[4] = coordinates[i+4];
+			textureCoordinates[5] = coordinates[i+5];
+			//top right
+			textureCoordinates[6] = coordinates[i+6];
+			textureCoordinates[7] = coordinates[i+7];
+					
+			ByteBuffer tcbb = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
+			tcbb.order(ByteOrder.nativeOrder());
+			texCoordBuffer[i/8] = tcbb.asFloatBuffer();
+			texCoordBuffer[i/8].put(textureCoordinates);
+			texCoordBuffer[i/8].position(0);
+		}
+		
+		
+		return texCoordBuffer; 
+	}
+	
+		public int createVBO(FloatBuffer vertexBuffer, FloatBuffer texCoordBuffer) {
+			
+			int vboId; 
+			int[] ids = new int[1];
+			GLES11.glGenBuffers(1, ids, 0);
+			vboId = ids[0];
+			
+			GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, vboId);
+			int size = (vertexBuffer.capacity()+texCoordBuffer.capacity())* 4; //4*3 Vertices, 4*2 Texcoords, 4 Byte je Float
+			GLES11.glBufferData(GLES11.GL_ARRAY_BUFFER, size, null, GLES11.GL_STATIC_DRAW);
+			
+			GLES11.glBufferSubData(GLES11.GL_ARRAY_BUFFER, 0, vertexBuffer.capacity()*4, vertexBuffer);
+			GLES11.glBufferSubData(GLES11.GL_ARRAY_BUFFER, vertexBuffer.capacity()*4, texCoordBuffer.capacity()*4, texCoordBuffer);
+			
+			
+			//GLES11.glBufferSubData(GLES11.GL_ELEMENT_ARRAY_BUFFER, (vertexBuffer.capacity() + texCoordBuffer.capacity())*4, indexBuffer.capacity()*2, indexBuffer);
+		
+			GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, 0);
+			
+			return vboId; 
+		}
+	
+	public static GeometryManager getInstance() {
+		if (geometryManager == null) 
+			geometryManager = new GeometryManager(); 
+		
+		return geometryManager; 
+	}
+}
