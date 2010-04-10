660e06c64a02081a68715205882a1d09c6979de8
email@reini.org <email@reini.org@77361afc-002d-11df-9d03-c52165109085>
2010-04-10 16:47:18 +0000

    Fürs erste wieder fehlerfrei

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level20/LevelActivity.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level20/LevelActivity.java
@@ -7,6 +7,8 @@ package at.ac.tuwien.cg.cgmd.bifth2010.level20;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * @author Reinbert
@@ -19,6 +21,9 @@ public class LevelActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		//Initiate our Lesson with this Activity Context handed over
 		renderView = new RenderView(this);
