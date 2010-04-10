732c86e62ea75fb72529153dca84a095fa1956b8
wcj.cgmd@gmail.com <wcj.cgmd@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-04-10 09:27:33 +0000

    Game loop prototype.

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level70/renderer/RendererView.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level70/renderer/RendererView.java
@@ -1,18 +1,29 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level70.renderer;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
+import java.util.LinkedList;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
-import android.opengl.GLSurfaceView.Renderer;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
+/**
+ * RendererView.
+ * @author herrjohann
+ */
 public class RendererView extends GLSurfaceView {
 
-	private RenderTask renderer; //< Renderer
+	// ----------------------------------------------------------------------------------
+	// -- Members ----
+	
+	private GameScene  scene;      //< Game scene
+	private RenderTask renderTask; //< Renderer
+	private UpdateTask updateTask; //< Renderer
+	
+	
+	// ----------------------------------------------------------------------------------
+	// -- Ctor / Dtor ----
 	
 	/**
 	 * Ctor.
@@ -22,32 +33,48 @@ public class RendererView extends GLSurfaceView {
 		super(context);
         setFocusable(true);
         
-        GeometryFactory gf = new GeometryFactory();
-		Geometry geom = gf.createQuad(1, 1);
+        scene = new GameScene();
         
-        renderer = new RenderTask(geom);
-        setRenderer(renderer);
+        renderTask = new RenderTask(scene);
+        setRenderer(renderTask);
            
-        Thread updTask = new Thread(new UpdateTask(geom));
+        updateTask = new UpdateTask(scene);
+        Thread updTask = new Thread(updateTask);
         updTask.start();
     }
 	
+	
+	// ----------------------------------------------------------------------------------
+	// -- Public methods ----
+	
+	/**
+	 * Add key down event to UpdateTask input queue.
+	 * @param The key code
+	 * @param The key event
+	 */
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-            return true;
-        }
-        Log.i("Input", "Key down pressed");
+		LinkedList<KeyEvent> inputs = updateTask.getInputs();
+		synchronized(inputs) {
+			inputs.add(event);
+		}
         return super.onKeyDown(keyCode, event);
     }
 	
+		
+	/**
+	 * Add key up event to UpdateTask input queue.
+	 * @param The key code
+	 * @param The key event
+	 */
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-            return true;
-        }
-        Log.i("Input", "Key up pressed");
-        return super.onKeyDown(keyCode, event);
+		LinkedList<KeyEvent> inputs = updateTask.getInputs();
+		synchronized(inputs) {
+			inputs.add(event);
+		}
+		return super.onKeyDown(keyCode, event);
     }
 
+	
 	public boolean  onTouchEvent(MotionEvent event) {
 		Log.i("Input", "Touch event");
 		return false;
