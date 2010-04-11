86895135c5f68945b1a6e8825107523ab994e4ff
tiaref26@gmail.com <tiaref26@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-04-11 17:40:51 +0000

    Added lvl60

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/Constants.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/Constants.java
@@ -35,7 +35,7 @@ public final class Constants {
 		return R.drawable.l00_levelicon_start;
 	}
 	
-	public static String[] LEVELIDS = {"11", "12", "13", "17", "20", "23", "30", "33", "36", "42", "44", "50", "55", "66", "70", "77", "83", "84", "88", "99"};
+	public static String[] LEVELIDS = {"11", "12", "13", "17", "20", "23", "30", "33", "36", "42", "44", "50", "55", "60", "66", "70", "77", "83", "84", "88", "99"};
 
 	
 }
