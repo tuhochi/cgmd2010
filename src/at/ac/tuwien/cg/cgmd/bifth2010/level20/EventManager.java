bf313d19ad196ad1392dd0d39e780a21aac07d4b
pilzinho@gmx.at <pilzinho@gmx.at@77361afc-002d-11df-9d03-c52165109085>
2010-04-13 23:34:46 +0000

    + added classes for event handling.

---
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level20/EventManager.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package at.ac.tuwien.cg.cgmd.bifth2010.level20;
+
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Every component can use this class to dispatch events to 
+ * the connected event listeners.
+ * 
+ * @see EventListener
+ * @author Pilzinho
+ *
+ */
+public class EventManager {
+	/**
+	 * The singleton instance of the event manager.
+	 */
+	private static final EventManager instance = new EventManager();
+	/**
+	 * The list of EventListeners that are registered with the manager.
+	 */	
+	protected List<EventListener> listeners;
+	
+	/**
+	 * The private constructor of the event manager.
+	 * Use getInstance() to get hold of the manager.
+	 */
+	private EventManager() {}
+
+	/**
+	 * Returns the singleton instance of the event manager.
+	 * @return	An instance of the EventManager class.
+	 */
+	public static EventManager getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * Adds an EventListener to the manager.
+	 * 
+	 * @param listener	The listener to add.
+	 * @return	True on success and false if the listener is already registered.
+	 */
+	public boolean addListener(EventListener listener) {
+		if(!listeners.contains(listener)) {
+			listeners.add(listener);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Removes an EventListener from the manager.
+	 * 
+	 * @param listener	The EventListener to remove from the manager.
+	 * @return	True on success and false if the listener is not in the list.
+	 */
+	public boolean removeListener(EventListener listener) {
+		if (!listeners.contains(listener)) {
+			return false;
+		}
+		listeners.remove(listener);
+		return true;		
+	}
+	
+	/**
+	 * Dispatches the event to the registered event listeners.
+	 * 
+	 * @param eventId 	The unique identifier of the event.
+	 * @param eventData	Data necessary for the listeners to handle the event properly.
+	 */
+	public void dispatchEvent(int eventID, Object eventData) {
+		ListIterator<EventListener> itr = listeners.listIterator();
+		while(itr.hasNext()) {
+			itr.next().handleEvent(eventID, eventData);
+		}
+	}
+	
+}
