0535cc332cf3ffe77cc85c8fa74d0db6235c7a33
matthias.tretter@gmail.com <matthias.tretter@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-04-13 17:41:48 +0000

    minor code clearage

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level44/twodee/CoinBucketSprite.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level44/twodee/CoinBucketSprite.java
@@ -2,17 +2,18 @@ package at.ac.tuwien.cg.cgmd.bifth2010.level44.twodee;
 
 import java.util.Random;
 
-import at.ac.tuwien.cg.cgmd.bifth2010.level44.physics.PhysicalRabbit;
-
 
 public class CoinBucketSprite extends SpriteContainer {
+	/** number of coins at the beginning */
+	private static final int FULL_COIN_COUNT = 10;
+	
 	public CoinBucketSprite(Texture texture) {
 		super(TextureParts.getBucket(texture));
 		setCenter(64, 0);
 		
 		Random random = new Random();
 		
-		for (int i=0; i<PhysicalRabbit.FULL_COIN_COUNT; i++) {
+		for (int i=0; i<FULL_COIN_COUNT; i++) {
 			Sprite coin = new Sprite(TextureParts.getCoin(texture));
 			coin.setPosition(random.nextInt(60)-30, 10+random.nextInt(10));
 			addChildFront(coin);
@@ -25,5 +26,9 @@ public class CoinBucketSprite extends SpriteContainer {
 		}
 		
 		return this.childrenFront.isEmpty();
+	}
+
+	public int getCoinCount() {
+		return childrenFront.size();
 	}
 }
