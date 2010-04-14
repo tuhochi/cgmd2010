30e86261daa015a7d80997f4c968157ecd55983c
pontomedon <pontomedon@77361afc-002d-11df-9d03-c52165109085>
2010-04-14 17:43:10 +0000

    l42 - updated to aom version 3

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level42/RenderView.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level42/RenderView.java
@@ -86,7 +86,7 @@ public class RenderView extends GLSurfaceView implements Renderer
 		//init temp vars
 		selectionDirection = new Vector3();
 		
-		scene = SceneLoader.getInstance().readScene("l42_cubeworld");
+		scene = SceneLoader.getInstance().readScene("l42_simpleorbit");
 		motionManager.generateRandomOrbit(scene,1,15,0,(float)Math.PI/4,0,(float)Math.PI/4,2,10);
 		collManager = new CollisionManager(scene);
 		
