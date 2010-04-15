91959996c124e7589531805a90eb8d8450441193
infamoustt@gmail.com <infamoustt@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-04-15 23:52:25 +0000

    kleine fixes bei sound und obstacles

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level23/util/SoundManager.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level23/util/SoundManager.java
@@ -6,15 +6,33 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.media.MediaPlayer;
 
+/**
+ * The Class manages audio output
+ * @author Markus Ernst
+ * @author Florian Felberbauer
+ */
 public class SoundManager 
 {
-	private Context context;
 	
-	private int numberOfPlayers=-1;
-	private ArrayList<MediaPlayer> players;
-	
+	/**
+	 * Instance of this Manager
+	 */
 	public static SoundManager instance;
 	
+	private Context context;
+			
+	/**
+	 * players currently initialized
+	 */
+	private ArrayList<MediaPlayer> players;
+	
+	
+	
+	
+	/**
+	 * Default Constructor
+	 * @param context
+	 */
 	public SoundManager(Context context)
 	{
 		this.context = context;
@@ -22,27 +40,43 @@ public class SoundManager
 		instance = this;
 	}
 	
+	/**
+	 * Request a media player
+	 * @param resId resource id for the player
+	 * @param isLooping true for player looping
+	 * @return playerId the id of the media player
+	 */
 	public int requestPlayer(int resId,boolean isLooping)
 	{
-		numberOfPlayers++;
 		MediaPlayer newPlayer = MediaPlayer.create(context,resId);
 		newPlayer.setLooping(true);
 		
 		players.add(newPlayer);
 		
-		return numberOfPlayers;
+		return players.size()-1;
 	}
 	
+	/**
+	 * Start a player with a specific id
+	 * @param id playerid of the player which will be started
+	 */
 	public void startPlayer(int id)
 	{
 		players.get(id).start();
 	}
 	
+	/**
+	 * Pause a player with a specific id
+	 * @param id playerid of the player which will be paused
+	 */
 	public void pausePlayer(int id)
 	{
 		players.get(id).pause();
 	}
 	
+	/**
+	 * Free media player resources
+	 */
 	public void releaseAudioResources()
 	{
 		for(int i=0;i<players.size();i++)
@@ -54,6 +88,9 @@ public class SoundManager
 		}
 	}
 	
+	/**
+	 * Pause all players
+	 */
 	public void pauseAllAudio()
 	{
 		for(int i=0;i<players.size();i++)
@@ -64,6 +101,9 @@ public class SoundManager
 		}
 	}
 		
+	/**
+	 * Reset the audio manager
+	 */
 	public void reset()
 	{
 		try {
