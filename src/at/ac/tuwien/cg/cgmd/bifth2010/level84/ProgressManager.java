10313208b1d35247ad4ff37bf9ceab5e4dc4e2f9
gerald.sollboeck <gerald.sollboeck@77361afc-002d-11df-9d03-c52165109085>
2010-04-16 14:01:56 +0000

    minor update for rohversion

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level84/ProgressManager.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level84/ProgressManager.java
@@ -5,19 +5,26 @@ import at.ac.tuwien.cg.cgmd.bifth2010.framework.SessionState;
 public class ProgressManager extends SessionState {
 
 	private int gamepoints = 0;
+	private int progress = 0; // must be between 0-100
+	
+	// the more difficult the figure of the gem is ...
+	// ... the more points you can lose ?!
 	
 	public void addPoints(int value)
 	{
+		gamepoints += value;
 	}
 	
 	public void losePoints(int value)
 	{
-		
+		gamepoints -= value;
 	}
 	
 	public void updateProgress(long time)
 	{
-		
+		//calc progress up to time ??
+		progress += 10;
+		setProgress(progress);
 	}
 	
 	public int getPoints()
