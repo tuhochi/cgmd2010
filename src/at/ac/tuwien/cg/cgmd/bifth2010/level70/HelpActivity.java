efe3ce5aa6853b2925da13fc32eb45a73dbb004c
wcj.cgmd@gmail.com <wcj.cgmd@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-04-18 16:07:13 +0000

    Thread synchronization. And Pipe game prototype.

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level70/HelpActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level70/HelpActivity.java
@@ -4,8 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import at.ac.tuwien.cg.cgmd.bifth2010.R;
 
+/**
+ * Help activity.
+ */
 public class HelpActivity extends Activity {
 
+	// ----------------------------------------------------------------------------------
+	// -- Public methods ----
+	
+	/**
+	 * Create help activity.
+	 * @param Bundle
+	 */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.l70_help);
