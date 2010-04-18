2f95859e8acb74b33d800eb8cbbff460a455d753
mike.vasiljevs@gmail.com <mike.vasiljevs@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-04-18 02:01:16 +0000

    textures added

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level77/L77Renderer.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level77/L77Renderer.java
@@ -25,20 +25,22 @@ public class L77Renderer implements Renderer {
 
     
 	// c++ inteface/adaptors
-    private static native void nativeInit();
+	private static native void nativeInit();
+	private static native void nativeInitClasses();    
     private static native void nativeDone();
     private static native void nativeRender();
     private static native void nativeResize(int w, int h);
     //debug
     private static native int nativeNumDrawn();
-    private static native int nativePushBitmap(int[] pixels, int w, int h);
 
     private boolean mTranslucentBackground;
     private Context mContext;
+    private Images mImages;
 
     public L77Renderer(boolean useTranslucentBackground, Context aContext) {
         mTranslucentBackground = useTranslucentBackground;
         mContext = aContext;
+        mImages = new Images(mContext);
     }
 
 	@Override
@@ -56,51 +58,10 @@ public class L77Renderer implements Renderer {
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {	
  		nativeInit();
  		
- 		loadBitmapTest();
+ 		mImages.loadImages();
+ 		
+ 		nativeInitClasses();
  		Log.i("renderer", "initiliased");
-	}
-	
-	public void loadBitmapTest()
-	{
-        InputStream is = mContext.getResources().openRawResource(R.drawable.l00_icon);
-
-        Bitmap bitmap;
-		try {
-		    bitmap = BitmapFactory.decodeStream(is);
-		} finally {
-		    try {
-		        is.close();
-		    } catch(IOException e) {
-		    }
-		}
-		
-		//...or Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.imgname);
-		//1st method: GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
-		//2nd method
-		int w = bitmap.getWidth();
-		int h = bitmap.getHeight();
-		
-		int[] pixels = new int[w * h]; 
-		bitmap.getPixels(pixels, 0, w, 0, 0, w, h); 
-		
-		for (int i = 0; i < w; i++)
-		{
-			for (int j = 0; j < h; j++)
-			{
-				if (i > 5 && i < (w - 5) &&
-						j > 5 && j < (h - 5))
-				{
-					pixels[j*w+i] = 0xff00ff00;
-				}
-				else
-				{
-					pixels[j*w+i] = 0x00770077;
-				}
-			}
-		}
-		
-		nativePushBitmap(pixels, w, h);
-		
 	}
 	
 	public void finalize()
