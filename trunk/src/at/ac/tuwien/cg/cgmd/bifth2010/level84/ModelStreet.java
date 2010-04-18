7080d8f354a85b900acc137fe8a52c7eaeca62dc
georg.molzer <georg.molzer@77361afc-002d-11df-9d03-c52165109085>
2010-04-18 19:33:41 +0000

    modelStreet (vorher: modelQuad

---
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level84/ModelStreet.java
@@ -0,0 +1,81 @@
+package at.ac.tuwien.cg.cgmd.bifth2010.level84;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.util.Log;
+import at.ac.tuwien.cg.cgmd.bifth2010.level17.math.Matrix4x4;
+
+public class ModelStreet extends Model {
+	
+	float qs = 3.0f; //quadsize
+	
+	/** Quad vertices */
+	protected float vertices[] = {
+			-qs, -qs, 1.0f, //v0
+	    	qs, -qs, 1.0f,  //v1
+	    	-qs, qs, 1.0f,  //v2
+	    	qs, qs, 1.0f,   //v3
+	};
+	/** Quad texcoords */
+	protected float texture[] = {
+			0.0f, 1.0f,
+	    	1.0f, 1.0f,
+	    	0.0f, 0.0f,
+	    	1.0f, 0.0f
+	};
+	/** Quad indices */
+	private byte indices[] = {0,1,3, 0,3,2};
+	
+	private float streetPos = 2.0f; //street position at startup
+	private float streetSpeed = 0.05f; //speed of street translation
+	private float streetLevel = -10f; //z-pos of street
+	
+	
+	/**
+	 * Creates a new quad.
+	 */
+	public ModelStreet() {
+		numIndices = indices.length;
+		
+		ByteBuffer byteBuf = ByteBuffer.allocateDirect(vertices.length * 4);
+		byteBuf.order(ByteOrder.nativeOrder());
+		vertexBuffer = byteBuf.asFloatBuffer();
+		vertexBuffer.put(vertices);
+		vertexBuffer.position(0);
+
+		byteBuf = ByteBuffer.allocateDirect(texture.length * 4);
+		byteBuf.order(ByteOrder.nativeOrder());
+		textureBuffer = byteBuf.asFloatBuffer();
+		textureBuffer.put(texture);
+		textureBuffer.position(0);
+
+		indexBuffer = ByteBuffer.allocateDirect(indices.length);
+		indexBuffer.put(indices);
+		indexBuffer.position(0);
+	}
+	
+	/**
+	 * Creates a new quad with an initial texture resource.
+	 * @param textureResource
+	 */
+	public ModelStreet(int textureResource) {
+		this();
+		this.textureResource = textureResource;
+	}
+	
+	
+	/**
+	 * Update the model's transformations.
+	 */
+	public void update(GL10 gl, double deltaTime) {
+		//mTrans = Matrix4x4.mult(Matrix4x4.RotateX((float)(1f * deltaTime)), mTrans);
+
+		gl.glPushMatrix();
+		streetPos -= streetSpeed;
+		gl.glTranslatef(streetPos, 0, streetLevel);
+		gl.glMultMatrixf(mTrans.toFloatArray(), 0);
+	}
+}
