7042746453cfdf3f6e6b3b00f027579a3121955c
felix.fleisz <felix.fleisz@77361afc-002d-11df-9d03-c52165109085>
2010-04-20 12:35:04 +0000

    timing pause resume

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level11/Timing.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level11/Timing.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class Timing {
 
     private static final String LOG_TAG = Timing.class.getSimpleName();
-	private float startTime, currTime, currFPS, lastFrameTime, deltaFrameTime;
+	private float startTime, currTime, currFPS, lastFrameTime, deltaFrameTime, pauseTimeStamp, pausedTime;
 	
 	public Timing() {
 		startTime = System.nanoTime()/1000000000.0f;
@@ -13,12 +13,13 @@ public class Timing {
 		lastFrameTime = 0.0f;
 		deltaFrameTime = 0.0f;
 		currFPS = 0.0f;
-		
+		pauseTimeStamp = 0.0f;
+		pausedTime = 0.0f;
 	}
 	
 	public void update() {
 		lastFrameTime = currTime;
-		currTime = (System.nanoTime()/1000000000.0f) - startTime;
+		currTime = (System.nanoTime()/1000000000.0f) - startTime - pausedTime;
 		deltaFrameTime = currTime - lastFrameTime;
 		currFPS = (Math.round((1.0f/deltaFrameTime)*100.0f))/100.0f;
 
@@ -37,6 +38,11 @@ public class Timing {
 		return currFPS;
 	}
 	
-	
+	public void pause(){
+		pauseTimeStamp = (System.nanoTime()/1000000000.0f);
+	}
+	public void resume(){
+		pausedTime += (System.nanoTime()/1000000000.0f)-pauseTimeStamp;
+	}
 	
 }
