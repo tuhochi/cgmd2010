8041768c1206ed814ff3536194fe89d66c92209b
email@reini.org <email@reini.org@77361afc-002d-11df-9d03-c52165109085>
2010-04-20 15:25:48 +0000

    Falscher Commit

--- cgmd2010/src/at/ac/tuwien/cg/cgmd/bifth2010/level17/VertexBufferType.java
+++
@@ -1,9 +0,0 @@
-package at.ac.tuwien.cg.cgmd.bifth2010.level17;
-
-public enum VertexBufferType {
-	Position,
-	TextureCoordinate,
-	Index,
-	Color,
-	Normal
-}
--- cgmd2010/src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/SceneEntity.java
+++
@@ -1,6 +0,0 @@
-package at.ac.tuwien.cg.cgmd.bifth2010.level23.entities;
-
-public interface SceneEntity 
-{
-	public void render();
-}
