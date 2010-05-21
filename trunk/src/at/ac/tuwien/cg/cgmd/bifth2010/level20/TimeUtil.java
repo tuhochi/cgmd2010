97b7a909bf731e5a5b41144d3677d5e306455a45
email@reini.org <email@reini.org@77361afc-002d-11df-9d03-c52165109085>
2010-05-21 04:59:40 +0000

    Hab die alte Version wieder hergestellt. Irgendwas hat da mit Branchen nicht funktioniert. Sorry

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level20/TimeUtil.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level20/TimeUtil.java
@@ -1,240 +1,202 @@
-package at.ac.tuwien.cg.cgmd.bifth2010.level20;
+package at.ac.tuwien.cg.cgmd.bifth2010.level23.util;
 
-//import java.util.ArrayList;
-//
-//import android.os.Bundle;
+import java.util.ArrayList;
+
+import android.os.Bundle;
 
 /**
  * The Class TimeUtil takes care of all tasks belonging to time.
- * Thanks to level23 for the first implementation of this class
- * 
  * @author Markus Ernst
  * @author Florian Felberbauer
- * @author Ferdinand Pilz
- * @author Reinhard Sprung
  */
 public class TimeUtil {
 		
 	/** The instance of TimeUtil to pass around. */
-	public static TimeUtil instance = new TimeUtil();
+	public static TimeUtil instance = new TimeUtil();;
 	
-//	/**
-//	 * Indicates if the TimeUtil was restored from Bundle
-//	 */
-//	private boolean wasRestored=false;
+	/**
+	 * Indicates if the TimeUtil was restored from Bundle
+	 */
+	private boolean wasRestored=false;
 	
 	/** The Time between to frames */
-	private long dt;
-	
-	/** The total time, the game has been running so far */
-	private long totalTime;
+	private float dt;
 	
 	/** The accumulated frame time. */
-	private long accFrameTime;
+	private float accFrameTime;
 	
 	/** The number of rendered frames. */
-	private int nrRenderedFrames;
+	private float nrRenderedFrames;
 	
 	/** The last frame time. */
 	private long lastFrameTime;
-	
-	/** The fps refresh interval in seconds. */
+	//in seconds
+	/** The fps refresh interval. */
 	private int fpsRefreshInterval = 3;
 	
 	/** The fps. */
 	private float fps;
 	
-//	/** The timer. */
-//	private ArrayList<TimingTask> scheduledTimers;
+	/** The timer. */
+	private ArrayList<TimingTask> scheduledTimers;
 		
 	/**
 	 * Instantiates a new time util.
 	 */
-	private TimeUtil() {
-//		scheduledTimers = new ArrayList<TimingTask>();
+	private TimeUtil()
+	{
+		scheduledTimers = new ArrayList<TimingTask>();
 		instance = this;
 	}
 		
 	//cause lvl20 is using it
-//	public static TimeUtil getInstance()
-//	{
-//		return instance;
-//	}
+	public static TimeUtil getInstance()
+	{
+		return instance;
+	}
 	
-//	/**
-//	 * Writing to Bundle 
-//	 * @param bundle Bundle to write to
-//	 */
-//	public void writeToBundle(Bundle bundle) 
-//	{
-//		bundle.putSerializable("timers", scheduledTimers);
-//	}
-//	
-//	/**
-//	 * Reading from Bundle 
-//	 * @param bundle Bundle to read from
-//	 */
-//	@SuppressWarnings("unchecked")
-//	public void readFromBundle(Bundle bundle) 
-//	{
-//		scheduledTimers = (ArrayList<TimingTask>)bundle.getSerializable("timers");
-//		wasRestored = true;
-//	}
-//	
-//	/**
-//	 * Schedules timer.
-//	 *
-//	 * @param task the TimerTask to schedule
-//	 */
-//	public void scheduleTimer(TimingTask task)
-//	{
-//		scheduledTimers.add(task);
-//	}
+	/**
+	 * Writing to Bundle 
+	 * @param bundle Bundle to write to
+	 */
+	public void writeToBundle(Bundle bundle) 
+	{
+		bundle.putSerializable("timers", scheduledTimers);
+	}
+	
+	/**
+	 * Reading from Bundle 
+	 * @param bundle Bundle to read from
+	 */
+	@SuppressWarnings("unchecked")
+	public void readFromBundle(Bundle bundle) 
+	{
+		scheduledTimers = (ArrayList<TimingTask>)bundle.getSerializable("timers");
+		wasRestored = true;
+	}
+	
+	/**
+	 * Schedules timer.
+	 *
+	 * @param task the TimerTask to schedule
+	 */
+	public void scheduleTimer(TimingTask task)
+	{
+		scheduledTimers.add(task);
+	}
 	
 	/**
 	 * Gets the dt.
 	 *
-	 * @return The dt
+	 * @return the dt
 	 */
-	public long getDt() {
+	public float getDt() {
 		return dt;
 	}
 	
 	/**
-	 * Gets the total time.
+	 * Sets the dt.
 	 *
-	 * @return The total time
+	 * @param dt the dt to set
 	 */
-	public long getTotalTime() {
-		return totalTime;
+	public void setDt(float dt) {
+		this.dt = dt;
+	}
+
+	/**
+	 * Gets the accumulated frame times.
+	 *
+	 * @return the accFrameTimes
+	 */
+	public float getAccFrameTimes() {
+		return accFrameTime;
 	}
 	
 	/**
-	 * Gets the total time in seconds.
+	 * Sets the accumulated frame time.
 	 *
-	 * @return The total time in seconds
+	 * @param accFrameTimes the accFrameTimes to set
 	 */
-	public float getTotalTimeInSeconds() {
-		return totalTime * 0.001f;
+	public void setAccFrameTime(float accFrameTimes) {
+		this.accFrameTime = accFrameTimes;
 	}
 	
-//	/**
-//	 * Sets the dt.
-//	 *
-//	 * @param dt the dt to set
-//	 */
-//	public void setDt(float dt) {
-//		this.dt = dt;
-//	}
-
-//	/**
-//	 * Gets the accumulated frame times.
-//	 *
-//	 * @return the accFrameTimes
-//	 */
-//	public long getAccFrameTimes() {
-//		return accFrameTime;
-//	}
-//	
-//	/**
-//	 * Sets the accumulated frame time.
-//	 *
-//	 * @param accFrameTimes the accFrameTimes to set
-//	 */
-//	public void setAccFrameTime(float accFrameTimes) {
-//		this.accFrameTime = accFrameTimes;
-//	}
-//	
-//	/**
-//	 * Gets the acc frame time in sec.
-//	 *
-//	 * @return the accFrameTimesInSec
-//	 */
-//	public float getAccFrameTimeInSec() {
-//		return accFrameTime/1000.0f;
-//	}
+	/**
+	 * Gets the acc frame time in sec.
+	 *
+	 * @return the accFrameTimesInSec
+	 */
+	public float getAccFrameTimeInSec() {
+		return accFrameTime/1000;
+	}
 	
-//	/**
-//	 * Gets the number of rendered frames.
-//	 *
-//	 * @return the nrRenderedFrames
-//	 */
-//	public float getNrRenderedFrames() {
-//		return nrRenderedFrames;
-//	}
+	/**
+	 * Gets the number of rendered frames.
+	 *
+	 * @return the nrRenderedFrames
+	 */
+	public float getNrRenderedFrames() {
+		return nrRenderedFrames;
+	}
 	
-//	/**
-//	 * Sets the number of rendered frames.
-//	 *
-//	 * @param nrRenderedFrames the nrRenderedFrames to set
-//	 */
-//	public void setNrRenderedFrames(float nrRenderedFrames) {
-//		this.nrRenderedFrames = nrRenderedFrames;
-//	}
+	/**
+	 * Sets the number of rendered frames.
+	 *
+	 * @param nrRenderedFrames the nrRenderedFrames to set
+	 */
+	public void setNrRenderedFrames(float nrRenderedFrames) {
+		this.nrRenderedFrames = nrRenderedFrames;
+	}
 		
 	/**
-	 * Gets the fps.
+	 * Gets the fPS.
 	 *
-	 * @return The fps
+	 * @return the fPS
 	 */
-	public float getFPS() {
+	public float getFPS()
+	{
 		return fps;
 	}
 	
 	/**
-	 * Updates the time.
+	 * Updates fps.
 	 */
-	public void update() {
+	public void update()
+	{
+		long currentTime = System.currentTimeMillis();
 		
-		long currentTime = System.currentTimeMillis();		
 		
+		
+		if(accFrameTime >= fpsRefreshInterval*1000)
+		{
+			fps = (nrRenderedFrames/accFrameTime)*1000;
+			accFrameTime=0;
+			nrRenderedFrames=0;
+		}
 			
 		//first frame
-		if (lastFrameTime == 0) {
-			lastFrameTime = currentTime;
-		} else {		
+		if(lastFrameTime == 0)
+			lastFrameTime=currentTime;
+		else
+		{		
 			dt = currentTime - lastFrameTime;
-			totalTime += dt;
 			accFrameTime += dt;
 			nrRenderedFrames++;
 			lastFrameTime = currentTime;
 		}
 		
-		if (accFrameTime >= fpsRefreshInterval * 1000) {
-			fps = ((float)nrRenderedFrames / accFrameTime) * 1000;
-			accFrameTime = 0;
-			nrRenderedFrames = 0;
+		for(int i=0;i<scheduledTimers.size();i++)
+		{
+			TimingTask tempTask = scheduledTimers.get(i);
+			if(tempTask.isDead)
+			{
+				scheduledTimers.remove(i);
+				tempTask.isDead = false;
+			}
+			else
+				scheduledTimers.get(i).update(dt);
 		}
-		
-//		for(int i=0;i<scheduledTimers.size();i++)
-//		{
-//			TimingTask tempTask = scheduledTimers.get(i);
-//			if(tempTask.isDead)
-//			{
-//				scheduledTimers.remove(i);
-//				tempTask.isDead = false;
-//			}
-//			else
-//				scheduledTimers.get(i).update(dt);
-//		}
 	}
-	
-//	/**
-//	 * Pauses the TimeUtil
-//	 */
-//	public void pause()
-//	{
-//		lastFrameTime = 0;
-//	}
-	
-//	/**
-//	 * Resumes the TimeUtil
-//	 */
-//	public void resume()
-//	{
-//		lastFrameTime = 0;
-//	}
-	
 	
 	/**
 	 * Resets the TimeUtil
@@ -242,19 +204,18 @@ public class TimeUtil {
 	public void reset()
 	{
 		lastFrameTime = 0;
-		dt = 0;
 	}
 	
-//	/**
-//	 * Resets the Timers holden by the TimeUtil
-//	 */
-//	public void resetTimers()
-//	{
-//		if(!wasRestored)
-//		{
-//			scheduledTimers.clear();
-//		}
-//		else
-//			wasRestored=false;
-//	}
+	/**
+	 * Resets the Timers holden by the TimeUtil
+	 */
+	public void resetTimers()
+	{
+		if(!wasRestored)
+		{
+			scheduledTimers.clear();
+		}
+		else
+			wasRestored=false;
+	}
 }
