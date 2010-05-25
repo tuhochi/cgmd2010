375abe356ab0d62c1fb7964163ceb10a1972de67
arthur.oberhauser@gmail.com <arthur.oberhauser@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-05-25 14:46:30 +0000

    mistress texture

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level13/MyRenderer.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level13/MyRenderer.java
@@ -76,7 +76,7 @@ public class MyRenderer extends GLSurfaceView implements Renderer {
 	public static int screenHeight;
 	
 	//general movement speed (must be a divisor of GameObject.BLOCKSIZE -> see todo in collisonhandler)
-	public static final float SPEED = 8f;
+	public static final float SPEED = 2f;
 	
 	private float zoomFactor = 1.0f;
 	private float zoom = 0.0f;
