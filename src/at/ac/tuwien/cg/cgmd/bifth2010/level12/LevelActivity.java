dea4a49e8388234dcbbbf161e998dd6f8e5d35fb
johannessorger@gmail.com <johannessorger@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-05-30 16:12:19 +0000

    l12 - gfx update

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level12/LevelActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level12/LevelActivity.java
@@ -68,6 +68,9 @@ public class LevelActivity extends Activity{
     	TextureManager.getSingletonObject().add(R.drawable.l12_icon);	
     	TextureManager.getSingletonObject().add(R.drawable.l12_road);		
     	TextureManager.getSingletonObject().add(R.drawable.l12_house);	
+    	TextureManager.getSingletonObject().add(R.drawable.l12_bunny1);	
+    	TextureManager.getSingletonObject().add(R.drawable.l12_bunny2);	
+    	TextureManager.getSingletonObject().add(R.drawable.l12_bunny3);	
     	TextureManager.getSingletonObject().add(R.drawable.l12_grass);
     	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
     	
--- src/at/ac/tuwien/cg/cgmd/bifth2010/level12/entities/BasicTower.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level12/entities/BasicTower.java
@@ -14,7 +14,8 @@ public class BasicTower extends Tower {
 		mColor[1] = 1.0f;
 		mColor[2] = 1.0f;
 		mColor[3] = 1.0f;
-		mTexture =  R.drawable.l12_basic_tower;
+		//mTexture =  R.drawable.l12_basic_tower;
+		mTexture =  R.drawable.l12_bunny1;
 		mPrice = Definitions.BASIC_TOWER_IRON_NEED;
 		mSound = R.raw.l12_basic_tower_shooting_sound;
 		SoundHandler.addSound(mSound);
