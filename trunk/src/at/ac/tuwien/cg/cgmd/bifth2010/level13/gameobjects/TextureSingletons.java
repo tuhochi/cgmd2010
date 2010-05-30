72e28a0f5e98ac844513ef8fe876d98782306f50
sebastian.sippl <sebastian.sippl@77361afc-002d-11df-9d03-c52165109085>
2010-05-30 18:06:29 +0000

    level 13: - added beer counter           - cops will only arrest you at 6 beers

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level13/gameobjects/TextureSingletons.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level13/gameobjects/TextureSingletons.java
@@ -35,6 +35,7 @@ public class TextureSingletons {
 		textures.put(MistressObject.class.getSimpleName(), new MistressTexture(gl, context));
 		textures.put(DrunkBar.class.getSimpleName(), new DrunkBarTexture(gl, context));
 		textures.put(JailBar.class.getSimpleName(), new JailBarTexture(gl, context));
+		textures.put(DrunkBarBlock.class.getSimpleName(), new DrunkBarBlockTexture(gl, context));
 	}
 	
 	/**
