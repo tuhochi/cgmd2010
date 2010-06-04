6cc6c3f92acc16c931f829aa9189eea635857b53
pontomedon <pontomedon@77361afc-002d-11df-9d03-c52165109085>
2010-06-04 21:45:08 +0000

    l42 - pulsing force implemented

---
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level42/util/MathUtil.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package at.ac.tuwien.cg.cgmd.bifth2010.level42.util;
+
+/**
+ * The Class MathUtil.
+ *
+ * @author Alex Druml
+ * @author Lukas Roessler
+ */
+public class MathUtil
+{
+	/**
+	 * Evaluates the following funktion:
+	 * sin(sin(t*PI/2)*PI-PI/2)/2+0.5
+	 * @param t the parameter of the function
+	 * @return	the result of the function
+	 */
+	public static native float forcePulseFunktion(float t);
+}
