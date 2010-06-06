80fd743b56535121bb905e25077c6b6228cef60f
infamoustt@gmail.com <infamoustt@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-06-06 22:37:23 +0000

    bg speed angepasst

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/Background.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level23/entities/Background.java
@@ -139,9 +139,9 @@ public class Background implements SceneEntity
 	public void update(float dt)
 	{
 		if(!repeat)
-			positionY -= dt*Settings.BALLOON_SPEED/4.0f;
+			positionY -= dt*Settings.BALLOON_SPEED/20.0f;
 		else
-			positionY -= (dt*Settings.BALLOON_SPEED/4.0f)/(RenderView.instance.getTopBounds()*2);
+			positionY -= (dt*Settings.BALLOON_SPEED/20.0f)/(RenderView.instance.getTopBounds()*2);
 //		if(positionY <= renderView.getTopBounds()*-1)
 //		{
 //			positionY += renderView.getTopBounds(); 
