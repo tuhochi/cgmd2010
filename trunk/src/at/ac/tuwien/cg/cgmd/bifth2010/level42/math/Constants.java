3bcf1b6e217a2df93708c53f4988709d66f45f3e
alex3.dr@gmail.com <alex3.dr@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-06-06 14:12:48 +0000

    l42 - doku update

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/math/Constants.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level42/math/Constants.java
@@ -11,12 +11,18 @@ public class Constants
 
 	/** The Constant TWOPI = Two times PI. */
 	public static final float TWOPI = (float)(Math.PI*2);
+	/** PI/2 */
 	public static final float PIHALF = (float)(Math.PI/2);
-	/** A few more Constant Vectors. */
-	public static final Vector3 X_AXIS = new Vector3(1,0,0), 
-								Y_AXIS = new Vector3(0,1,0), 
-								Z_AXIS = new Vector3(0,0,1),
-								ZERO_VEC = new Vector3(0,0,0),
-								DUMMY_VARIATION_VEC = new Vector3(0.01f,0.01f,0.01f),
-								DUMMY_INIT_VEC = new Vector3(0,0,0.1f);
+	/** The X Axis */
+	public static final Vector3 X_AXIS = new Vector3(1,0,0); 
+	/** The Y Axis. */
+	public static final Vector3 Y_AXIS = new Vector3(0,1,0); 
+	/** The Z Axis */
+	public static final Vector3 Z_AXIS = new Vector3(0,0,1);
+	/** The Zero vector */
+	public static final Vector3 ZERO_VEC = new Vector3(0,0,0);
+	/** A dummy deflaction vector */
+	public static final Vector3 DUMMY_VARIATION_VEC = new Vector3(0.01f,0.01f,0.01f);
+	/** A dummy initialization vector */
+	public static final Vector3 DUMMY_INIT_VEC = new Vector3(0,0,0.1f);
 }
