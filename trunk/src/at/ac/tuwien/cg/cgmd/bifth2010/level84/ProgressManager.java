83b808cf5d1a236d3200dd1e088647ef376e8dee
gerald.sollboeck <gerald.sollboeck@77361afc-002d-11df-9d03-c52165109085>
2010-06-08 10:30:17 +0000

    - credits completed - resultdialog update

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level84/ProgressManager.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level84/ProgressManager.java
@@ -10,6 +10,9 @@ public class ProgressManager extends SessionState {
 	private int totalValue;
 	private Activity activity;
 	
+	private int[] gemsHit = new int[3];
+	private int[] gemsMiss = new int[3];
+	
 	public ProgressManager(Activity activity) {
 		this.activity = activity;
 	}
@@ -36,11 +39,13 @@ public class ProgressManager extends SessionState {
 	 */
 	public void loseMoneyByHit(int drainType) {
 		remainingValue -= drainType * LevelActivity.GEM_BASE_VALUE;
+		updateGemStatsByHit(drainType);
 		if (remainingValue <= 0) {
 			remainingValue = 0;
 			this.activity.finish();
 		}
 	}
+	
 	
 	/**
 	 * define how much money is lost dependent on the draintype if the gem breaks
@@ -49,10 +54,31 @@ public class ProgressManager extends SessionState {
 	public void loseMoneyByBreak(int drainType)
 	{
 		remainingValue -= drainType * LevelActivity.GEM_BASE_VALUE / 10;
+		updateGemStatsByMiss(drainType);
 		if (remainingValue <= 0) {
 			remainingValue = 0;
 			this.activity.finish();
 		}
+	}
+	
+	private void updateGemStatsByHit(int drainType)
+	{
+		gemsHit[drainType]++;
+	}
+	
+	private void updateGemStatsByMiss(int drainType)
+	{
+		gemsMiss[drainType]++;
+	}
+	
+	public int getGemStatsHit(int drainType)
+	{
+		return gemsHit[drainType-1];
+	}
+	
+	public int getGemStatsMiss(int drainType)
+	{
+		return gemsMiss[drainType-1];
 	}
 	
 	public int getProgress()
