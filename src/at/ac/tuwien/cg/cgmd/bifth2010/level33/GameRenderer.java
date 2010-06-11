dfe927d92ed64f5a532f8ceff14e00151beccaae
0625267 <0625267@77361afc-002d-11df-9d03-c52165109085>
2010-06-11 15:42:40 +0000

    Credits hinzugefügt für Level33; Tutorial hinzugefügt, LadeScreen, Images für Ladescreen hinzugefügt

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level33/GameRenderer.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level33/GameRenderer.java
@@ -1,11 +1,13 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level33;
 
 import static android.opengl.GLES10.GL_MODELVIEW;
+
 import static android.opengl.GLES10.GL_PROJECTION;
 import static android.opengl.GLES10.glLoadIdentity;
 import static android.opengl.GLES10.glMatrixMode;
 import static android.opengl.GLES10.glViewport;
 import static android.opengl.GLU.gluPerspective;
+import at.ac.tuwien.cg.cgmd.bifth2010.R;
 import at.ac.tuwien.cg.cgmd.bifth2010.level33.math.Vector2f;
 import at.ac.tuwien.cg.cgmd.bifth2010.level33.math.Vector2i;
 import at.ac.tuwien.cg.cgmd.bifth2010.level33.scene.Camera;
@@ -18,16 +20,49 @@ import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
 import android.util.Log;
 
-public class GameRenderer implements GLSurfaceView.Renderer {
 
-	public GameRenderer() {
+
+public class GameRenderer implements GLSurfaceView.Renderer {
+	
+	LevelActivity levelActivity = null;
+	SceneGraph sg = null;
+	boolean isLoadingScreenActive=false;
+	public static boolean SWITCH_VIEW = false;
+
+	public GameRenderer(LevelActivity levelActivity, SceneGraph sg) {
 		// TODO Auto-generated constructor stub
 		Log.d("GameRenderer","constructor");
+		this.levelActivity = levelActivity;
+		this.sg = sg;
 	}
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		LevelActivity.sceneGraph.render(gl);
+		
+		if(LevelActivity.GAME_STATE.equals(LevelActivity.AllGameStates.PLAYING))
+		{
+			if(SWITCH_VIEW)
+			{
+				sg.switchView();
+				SWITCH_VIEW=false;
+			}
+			
+			LevelActivity.sceneGraph.render(gl);
+		}
+		
+		if(LevelActivity.GAME_STATE.equals(LevelActivity.AllGameStates.LOADING))
+		{
+			if(!isLoadingScreenActive)
+				isLoadingScreenActive=true;
+			else
+			{
+				//Init all things
+				SceneGraph.init(gl); // now init the Geometry VBO´s
+				
+				LevelActivity.GAME_STATE=LevelActivity.AllGameStates.READY;
+				sg.loadingComplete();
+			}
+		}
 	}
 
 	@Override
@@ -42,11 +77,6 @@ public class GameRenderer implements GLSurfaceView.Renderer {
 		glLoadIdentity();
 		gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
 		LevelActivity.resolution = new Vector2f(width, height);
-		
-		
-		
-		
-		
 		
 		// set Game Frustum
 		double x =Camera.standardZoom/Math.sqrt(2)*(LevelActivity.resolution.x/LevelActivity.resolution.y);
@@ -68,9 +98,17 @@ public class GameRenderer implements GLSurfaceView.Renderer {
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		
+		//levelActivity.viewSwitcher.getNextView();
+		//levelActivity.setContentView(R.layout.l33_loading_screen);
+		
 		Log.d("onSurfaceCreated","jo");
-		SceneGraph.init(gl); // now init the Geometry VBO´s
+		//SceneGraph.init(gl); // now init the Geometry VBO´s
 		Log.d("onSurfaceCreated","fin");
+		
+		//levelActivity.setContentView(R.layout.l33_level);
+		
+		
 		
 	}
 
