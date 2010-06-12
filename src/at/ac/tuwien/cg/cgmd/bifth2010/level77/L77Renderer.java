cc9973e0d015b76b926587b811c10811c837251e
gkatze82@gmail.com <gkatze82@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-06-12 12:57:35 +0000

    CoinBar and buggy timerBar

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level77/L77Renderer.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level77/L77Renderer.java
@@ -10,6 +10,7 @@ import android.opengl.GLU;
 import android.opengl.GLUtils;
 import android.opengl.GLSurfaceView.Renderer;
 import android.util.Log;
+import android.view.MotionEvent;
 import at.ac.tuwien.cg.cgmd.bifth2010.R;
 import at.ac.tuwien.cg.cgmd.bifth2010.framework.Cylinder;
 import at.ac.tuwien.cg.cgmd.bifth2010.framework.Rectangle;
@@ -24,29 +25,32 @@ import android.graphics.BitmapFactory;
  */
 public class L77Renderer implements Renderer
 {
-	private boolean	mTranslucentBackground;
 	private Context	mContext;
 	private Native	jni;
 	private Rectangle timeBar;
-	private Rectangle goldBar;
-	private Rectangle barBorder;
+	private CoinBar coinBar;
+	private long startTime;
+	
+	private static String TAG = "L77Renderer";
+	private Callback<Void> timeUp;
 
 	/**
 	 * constructor for Level 77 Renderer
 	 * 
 	 * @param useTranslucentBackground
 	 * @param aContext
+	 * @param timeUp 
 	 */
-	public L77Renderer(boolean useTranslucentBackground, Context aContext, Native native_jni)
+	public L77Renderer(boolean useTranslucentBackground, Context aContext, Native native_jni, Callback<Void> timeUp)
 	{
-		mTranslucentBackground = useTranslucentBackground;
 		mContext = aContext;
 		jni = native_jni;
+		this.timeUp = timeUp;
 	}
 
-	private float		mAngle	= 0.0f;
-	private Cylinder	mCube;
 
+	private float percent = 1.0f;
+	
 	/**
 	 * TODO Javadoc
 	 */
@@ -63,36 +67,20 @@ public class L77Renderer implements Renderer
 		gl.glMatrixMode(GL10.GL_MODELVIEW);
 		gl.glLoadIdentity();
 		
-		gl.glTranslatef(6.75f, 4.5f, 5.0f);
-		gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-		//barBorder.draw(gl);
 		
-		gl.glTranslatef(-0.25f, 0, -0.5f);
-		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);		
+		gl.glTranslatef(6.5f, 0.0f, 5.0f);
+		coinBar.draw(percent);
+			
+		gl.glTranslatef(0.5f, 4.5f, 0.0f);
+		long currentTime = System.currentTimeMillis();
+		float timePercent = 1.0f - ((float) (currentTime - startTime)) / 1000f / 120f; 
+//		if (timePercent <= 0)
+//			timeUp.onSucces(null);
+		
+		gl.glScalef(1.0f, timePercent, 1.0f);
+		gl.glColor4f(1, 1, 1, 1);
 		timeBar.draw(gl);
 		
-		gl.glTranslatef(0.5f, 0.0f, 0.0f);
-		
-		timeBar.draw(gl);
-		
-//		gl.glTranslatef(0, 0, -3.0f);
-//		gl.glRotatef(mAngle, 0, 1, 0);
-//		gl.glRotatef(mAngle * 0.25f, 1, 0, 0);
-//
-//		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-//		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-//
-//		mCube.draw(gl);
-//
-//		gl.glRotatef(mAngle * 2.0f, 0, 1, 1);
-//		gl.glTranslatef(0.5f, 0.5f, 0.5f);
-//
-//		mCube.draw(gl);
-//
-//		gl.glBindTexture(GL10.GL_TEXTURE_2D, 0);
-//
-//		mAngle += 1.2f;
-
 		gl.glFlush();
 	}
 
@@ -111,22 +99,47 @@ public class L77Renderer implements Renderer
 		jni.init();
 		jni.init2();
 
-		timeBar = new Rectangle(0.5f, 9.0f);
-		timeBar.setTexture(gl, mContext.getResources(), R.drawable.l77_time_bar);
-		
-		barBorder = new Rectangle(1.0f, 9.0f);
-		
+		timeBar = new Rectangle(0.25f, 9.0f);
+		timeBar.setTexture(gl, mContext.getResources(), R.drawable.l77_time_bar);	
+		coinBar = new CoinBar(0.5f, 9.0f, 0.1f, -42.0f, mContext, gl);
 
-		
-		
-		mCube = new Cylinder(0.5f, 0.10f, (short) 10);
-		mCube.setTexture(gl, mContext.getResources(), R.drawable.l00_coin);
-		mCube.setColor(0.85f, 0.68f, 0.22f, 1.f);
 	}
 
 	public void finalize()
 	{
 		jni.deInit();
+	}
+
+	private float dragStartX = 0, dragStartY = 0, lastDragX = 0, lastDragY = 0;
+	
+	public void onTouchEvent(MotionEvent event)
+	{
+		//
+		float x = event.getX(), y = event.getY();
+		
+		if (event.getAction() == MotionEvent.ACTION_DOWN)
+		{
+			dragStartX = lastDragX = x;
+			dragStartY = lastDragY = y;
+		}
+		
+		if (event.getAction() == MotionEvent.ACTION_MOVE)
+		{
+			float distanceX = x - lastDragX;
+			float distanceY = y - lastDragY;
+			lastDragX = x;
+			lastDragY = y;
+			
+			
+			percent = percent - distanceX / 100.0f;
+			Log.d(TAG, "percent: " + percent);
+
+		}
+	}
+
+	public void setStartTime(long startTime)
+	{
+		this.startTime = startTime;
 	}
 
 }
