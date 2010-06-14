b963af105378783e266fbc40f70bdd89c7c0e0f6
infamoustt@gmail.com <infamoustt@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-06-14 16:33:00 +0000

    score berechnung geaendert

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level23/util/Settings.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level23/util/Settings.java
@@ -48,7 +48,7 @@ public class Settings
 	public static final float MAINCHAR_STARTPOSY = 3f;
 	
 	/** Will be multiplied with balloonHeight for Score */
-	public static final float SCOREHEIGHT_MODIFIER = 0.0165f;
+	public static final float SCOREHEIGHT_MODIFIER = 0.0175f;
 	
 	public static final int MAX_HEIGHT = 5000;
 	
