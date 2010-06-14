0ec5f1c3657c40b01e3a3702448af50ea6cd8215
dreimalbla@gmail.com <dreimalbla@gmail.com@77361afc-002d-11df-9d03-c52165109085>
2010-06-14 14:59:57 +0000

    l11 - JavaDoc updates

--- trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level11/Color.java
+++ trunk/src/at/ac/tuwien/cg/cgmd/bifth2010/level11/Color.java
@@ -1,13 +1,12 @@
 package at.ac.tuwien.cg.cgmd.bifth2010.level11;
 
-import android.util.Log;
-
-
-
+/**
+ * Color Object, predefined skin colors are caucasian, asian, african, p
+ * redefined hair colors are blonde black and brown;
+ * @author g11
+ *
+ */
 public class Color {
-
-    private static final String LOG_TAG = Color.class.getSimpleName();
-
     public static final Color caucasian = new Color(0.816f, 0.707f, 0.641f, 1.0f);
     public static final Color asian = new Color (0.805f, 0.732f, 0.508f, 1.0f);
     public static final Color african = new Color (0.473f, 0.324f, 0.129f, 1.0f);
@@ -17,16 +16,23 @@ public class Color {
     
 	float r, g, b, a;
 	
-	
+	/**
+	 * Creates a white color.
+	 */
 	public Color() {
-		//Log.i(LOG_TAG, "Color()");
-		
 		this.r = 1.0f;
 		this.g = 1.0f;
 		this.b = 1.0f;
 		this.a = 1.0f;
 	}
 	
+	/**
+	 * Creates a specific color
+	 * @param r red 0.0 to 1.0
+	 * @param g green 0.0 to 1.0
+	 * @param b blue 0.0 to 1.0
+	 * @param a alpha 0.0 to 1.0
+	 */
 	public Color(float r, float g, float b, float a) {
 		this.r = r;
 		this.g = g;
@@ -34,11 +40,17 @@ public class Color {
 		this.a = a;
 	}
 	
+	/**
+	 * Sets r, g, b and a to a specific value.
+	 * @param r red 0.0 to 1.0
+	 * @param g green 0.0 to 1.0
+	 * @param b blue 0.0 to 1.0
+	 * @param a alpha 0.0 to 1.0
+	 */
 	public void set(float r, float g, float b, float a) {
 		this.r = r;
 		this.g = g;
 		this.b = b;
 		this.a = a;
-	}
-	
+	}	
 }
