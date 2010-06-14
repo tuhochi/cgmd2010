55f0dba41d0913ff951e1d08925d1dd30243cadf
toepmark <toepmark@77361afc-002d-11df-9d03-c52165109085>
2010-06-14 15:58:47 +0000

    doku

--- src/at/ac/tuwien/cg/cgmd/bifth2010/level12/LevelActivity.java
+++ src/at/ac/tuwien/cg/cgmd/bifth2010/level12/LevelActivity.java
@@ -27,17 +27,27 @@ import at.ac.tuwien.cg.cgmd.bifth2010.R;
 import at.ac.tuwien.cg.cgmd.bifth2010.framework.SessionState;
 
 
+/**
+ * Activity which starts and ends the level
+ * @see android.app.Activity
+ */
+
 public class LevelActivity extends Activity{
-	private Display mDisplay = null;
-	private GLRenderer mRenderer = null;
-	private boolean mTutShowed = false;
-	private LinearLayout mL;
-	private Context mContext = this;
-	private Handler mFinish;
-	private boolean mFinished = false;
+	
+	private Display mDisplay = null; /** display to operate on */
+	private GLRenderer mRenderer = null; /** OpenGL Renderer class */
+	private boolean mTutShowed = false; /** Was the Tutorial dialog shown? */
+	private LinearLayout mL; /** Layout which holds the GLSurfaceView and the GameUI */  
+	private Context mContext = this; /** pointer to the level activity itself which is also the app context */  
+	private Handler mFinish; /** Handler for showing the finish dialog and finishing the game */
+	private boolean mFinished = false; /** is the game finished? */
 	
 	
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. 
+     * Inherited from the android.app.Activiy class, sets the level to fullscreen and created the handler which gets called when the game is finished.
+     * 
+     *@param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	//Debug.startMethodTracing("Tracefile");
@@ -63,6 +73,10 @@ public class LevelActivity extends Activity{
         
     }
     
+    /**
+     * Inherited from android.app.Activity
+     * Shows the Tutorial dialog
+     */
     @Override
     public void onStart(){   
         super.onStart();
@@ -74,6 +88,11 @@ public class LevelActivity extends Activity{
         }     
     }
     
+    /**
+     * Inherited from android.app.Activity
+     * Receives the touch events done by the user on the touchscreen and forwards them to the GameWorld singleton.
+     * @param event the touch event
+     */
     @Override
     public boolean onTouchEvent (MotionEvent event)
     {
@@ -86,7 +105,13 @@ public class LevelActivity extends Activity{
     }
     
     
-    
+    /**
+     * Inherited from android.app.Activity
+     * Resumes the level activity by setting the sound to on/off depending on the framework boolean , 
+     * setting the screen to landscape mode, setting the height for the GameUI and the GLSurfaceView,
+     *  forwarding the display to the GameWorld, (re)creating the OpenGL VBOs, (re)loading the soundsamples, and initialising and showing the View (=Linear Layout).
+     * 
+     */
     @Override
     protected void onResume() {
     	SoundHandler.setContext(this);
@@ -96,7 +121,6 @@ public class LevelActivity extends Activity{
 		if(state!=null){
 			boolean isMusicAndSoundOn = state.isMusicAndSoundOn(); 
 			SoundHandler.getSingleton().setSound(isMusicAndSoundOn);
-			System.out.println("SETTING SOUND TO: "+isMusicAndSoundOn);
 		}
     	
     	//SoundHandler.getSingleton().addResource(R.raw.l12_music);
@@ -127,7 +151,10 @@ public class LevelActivity extends Activity{
         super.onResume();
     }
     
-    
+    /**
+     * Inherited from android.app.Activity
+     * Pausing the game by forwarting the pause call to the GameMechanics singleton and the SoundHandler singleton
+     */
     @Override
     protected void onPause() {
     	GameMechanics.getSingleton().pause();
@@ -135,6 +162,10 @@ public class LevelActivity extends Activity{
         super.onPause();
     }
     
+    /**
+     * Inherited from android.app.Activity
+     * Stopping the app
+     */
     @Override
 	protected void onStop() {
 		//we finish this activity;
@@ -142,6 +173,10 @@ public class LevelActivity extends Activity{
     	//Debug.stopMethodTracing();
     }
     
+    
+    /**
+     * Method responsible for showing the finish dialog, sends an empty message to the Handler which handles creation, shows and dismisses the dialog
+     */
     public void showFinishDialog(){
     	if( !mFinished ){
     		mFinished = true;
@@ -149,25 +184,12 @@ public class LevelActivity extends Activity{
     	}
     }
     
+    /**
+     * Inherited from android.app.Activity
+     * Finishes the level activity and returns the level result to the framework
+     */
 	@Override
 	public void finish() {
-		System.out.println("FINISH");
-		
-		System.out.println("REMOVE VIEWS");
-		//mL.removeAllViews();
-		System.out.println("REMOVE VIEWS1");
-		//mL.removeAllViewsInLayout();
-		System.out.println("REMOVE VIEWS2");
-		
-		/*TextView t = new TextView(this);
-		t.setText(R.string.l12_enemies_fended);
-		mL.addView(t);
-		TextView t1 = new TextView(this);
-		t1.setText(mKilledEnemies + " / "+mSpawnedEnemies+"\n\n" );
-		mL.addView(t1);
-		this.setContentView(mL);
-		System.out.println("Content View Set!");*/
-		
 		//the SessionState is a convenience class to set a result
 		SessionState s = new SessionState();
 		//we set the progress the user has made (must be between 0-100)
@@ -177,7 +199,6 @@ public class LevelActivity extends Activity{
 		if(totalMoneyPercent == 0.0f) s.setProgress(0);
 		else s.setProgress((int)(burnedMoney/totalMoneyPercent));
 		
-		//System.out.println("burned: " + burnedMoney + " |gained: " + gainedMoney + "score: " + (int)(burnedMoney/totalMoneyPercent));
 		//we call the activity's setResult method 
 		setResult(Activity.RESULT_OK, s.asIntent());
 		GameWorld.destroySingleton();
@@ -187,6 +208,11 @@ public class LevelActivity extends Activity{
 		mDisplay = null;
 	}
 	
+	
+	/**
+	 * Inherited from android.app.Activity
+	 * Destroying the game context
+	 */
 	@Override
 	public void onDestroy(){
 	   	super.onDestroy();
@@ -195,9 +221,15 @@ public class LevelActivity extends Activity{
 	
 	    
 	
-	
+	/**
+	 * The Tutorial dialog, inherity from AlertDialog and implements an OnClickListener for the start button.
+	 * @see android.app.AlertDialog
+	 * @see  android.view.View.OnClickListener
+	 */
 	private class StartDialog extends AlertDialog implements android.view.View.OnClickListener{
-
+		private Button mBtn; /** start button */
+		
+		/** Creates and shows the Layout */
 		protected StartDialog(Context context) {
 			super(context);
 			this.setTitle(R.string.l12_tutorialtitle);
@@ -252,26 +284,37 @@ public class LevelActivity extends Activity{
 			liv.addView(t4);
 			l.addView(liv);
 			
-			Button btn = new Button(context);
-			btn.setText("Start");
-			btn.setId(1);
-			btn.setOnClickListener(this);
-			l.addView(btn);
+			mBtn = new Button(context);
+			mBtn.setText("Start");
+			mBtn.setId(1);
+			mBtn.setOnClickListener(this);
+			l.addView(mBtn);
 				
 			sc.addView(l);	
 			this.setView(sc);
 		}
 
+		/** received the click events, starts the game (unpauses it) and dismisses the dialog */
 		@Override
 		public void onClick(View v) {
-			GameMechanics.getSingleton().unpause();
-			this.dismiss();	
+			if( v.getId() == mBtn.getId() ){
+				GameMechanics.getSingleton().unpause();
+				this.dismiss();	
+			}
 		}
 	}
 	
+	/**
+	 * Dialog shown at the end of the game, inherits from AlertDialog, implements an OnClickListener for the End button
+	 * @see android.app.AlertDialog
+	 * @see  android.view.View.OnClickListener
+	 */
 	private class FinishDialog  extends AlertDialog  implements android.view.View.OnClickListener{
-		Button mBtn;
+		Button mBtn; /** end button */
 
+		/**
+		 * creates and shows the layout for the dialog
+		 */
 		protected FinishDialog(Context context) {
 			super(context);
 			this.setIcon(R.drawable.l12_icon);
@@ -340,7 +383,8 @@ public class LevelActivity extends Activity{
 			this.setView(sc);
 			
 		}
-
+		
+		/** receives and handles the click event on the button and dismisses the dialog */
 		@Override
 		public void onClick(View v) {
 			if( v.getId() == mBtn.getId() ) this.dismiss();
